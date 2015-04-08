10b2a209b909bf8123d75d8baf314cb2f079933c
Rohit Singhal <rohitsinghal@Rohits-MacBook-Pro.local>
2015-04-08 14:02:01 +0530

    version bump

--- src/com/bsb/hike/analytics/AnalyticsConstants.java
+++ src/com/bsb/hike/analytics/AnalyticsConstants.java
@@ -230,7 +230,7 @@ public class AnalyticsConstants
 	
 	//----------------------***----------------------------------//
 	
-	public static final String MSISDN = "msdn";
+	public static final String MSISDN = "t_user";
 	
 	public static final String TRACK_ID = "track_id";
 	
