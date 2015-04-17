98c397c04b4964b6ee6c734afa35e5b9eb8dbe24
Sidharth <sidharth@hike.in>
2015-04-17 23:56:18 +0530

    fixed stack overflow error on contact manager initialisation

--- src/com/bsb/hike/modules/contactmgr/ContactManager.java
+++ src/com/bsb/hike/modules/contactmgr/ContactManager.java
@@ -69,7 +69,7 @@ public class ContactManager implements ITransientCache, HikePubSub.Listener
 
 	private Context context;
 
-	private String[] pubSubListeners = { HikePubSub.APP_BACKGROUNDED };
+	private static String[] pubSubListeners = { HikePubSub.APP_BACKGROUNDED };
 
 	private ContactManager()
 	{
@@ -77,11 +77,6 @@ public class ContactManager implements ITransientCache, HikePubSub.Listener
 		hDb = HikeUserDatabase.getInstance();
 		persistenceCache = new PersistenceCache(hDb);
 		transientCache = new TransientCache(hDb);
-
-		// Called to set name for group whose group name is empty (group created by ios) , we cannot do this inside persistence cache load memory because at taht point transient
-		// and persistence cache have not been initialized completely
-		persistenceCache.updateGroupNames();
-		HikeMessengerApp.getPubSub().addListeners(this, pubSubListeners);
 	}
 
 	public static ContactManager getInstance()
@@ -93,12 +88,27 @@ public class ContactManager implements ITransientCache, HikePubSub.Listener
 				if (_instance == null)
 				{
 					_instance = new ContactManager();
+					init();
 				}
 			}
 		}
 		return _instance;
 	}
 
+	private static void init()
+	{
+		if (_instance == null)
+		{
+			throw new IllegalStateException("Contact Manager getInstance() should be called that will create a new instance and initialize");
+		}
+
+		HikeMessengerApp.getPubSub().addListeners(_instance, pubSubListeners);
+
+		// Called to set name for group whose group name is empty (group created by ios) , we cannot do this inside persistence cache load memory because at taht point transient
+		// and persistence cache have not been initialized completely
+		_instance.persistenceCache.updateGroupNames();
+	}
+	
 	public SQLiteDatabase getWritableDatabase()
 	{
 		return hDb.getWritableDatabase();
@@ -2151,7 +2161,9 @@ public class ContactManager implements ITransientCache, HikePubSub.Listener
 	 */
 	public void refreshContactManager()
 	{
+		// clear persistence, transient cache and assign null to all other class members
 		shutdown();
+		// create a new instance and initialize it
 		getInstance();
 	}
 
