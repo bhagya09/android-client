09925c28393b27f377013a9be96ca2527bca4b90
Akhil-Tripathi <akhiltripathi@Akhils-MacBook-Pro.local>
2015-04-20 13:41:21 +0530

    Bug Fixes
    - Squared DP getting uploaded while setting DP from photos flow
    - Select your preview text missing on preview screeSelect your preview
    text missing on preview screen

--- src/com/bsb/hike/ui/GalleryActivity.java
+++ src/com/bsb/hike/ui/GalleryActivity.java
@@ -241,7 +241,7 @@ public class GalleryActivity extends HikeAppStateBaseFragmentActivity implements
 		{
 			File selectedFile = Utils.createNewFile(HikeFileType.IMAGE, HikeConstants.CAM_IMG_PREFIX);
 			Intent sourceIntent = IntentFactory.getNativeCameraAppIntent(true,selectedFile);
-			Intent desIntent = IntentFactory.getPictureEditorActivityIntent(GalleryActivity.this, null, false);
+			Intent desIntent = IntentFactory.getPictureEditorActivityIntent(GalleryActivity.this, null, !returnResult);
 
 			Intent proxyIntent = new Intent(GalleryActivity.this, DelegateActivity.class);
 			proxyIntent.putExtra(DelegateActivity.SOURCE_INTENT, sourceIntent);
@@ -762,7 +762,7 @@ public class GalleryActivity extends HikeAppStateBaseFragmentActivity implements
 			File selectedFile = Utils.createNewFile(HikeFileType.IMAGE, HikeConstants.CAM_IMG_PREFIX);
 				
 			Intent sourceIntent = IntentFactory.getNativeCameraAppIntent(true,selectedFile);
-			Intent desIntent = IntentFactory.getPictureEditorActivityIntent(GalleryActivity.this, null, false);
+			Intent desIntent = IntentFactory.getPictureEditorActivityIntent(GalleryActivity.this, null, !returnResult);
 
 			Intent proxyIntent = new Intent(GalleryActivity.this, DelegateActivity.class);
 			proxyIntent.putExtra(DelegateActivity.SOURCE_INTENT, sourceIntent);
