2fb367d487652ae91bb9c0ba8bf536b730a0c93e
Shobhit <shobhit@Shobhits-MacBook-Pro.local>
2015-04-21 18:54:47 +0530

    platform changes for ctr

--- src/com/bsb/hike/chatthread/ChatThread.java
+++ src/com/bsb/hike/chatthread/ChatThread.java
@@ -700,6 +700,10 @@ public abstract class ChatThread extends SimpleOnGestureListener implements Over
 				mConversationsView.requestFocusFromTouch();
 				mConversationsView.setSelection(messages.size() - 1);
 			}
+			break;
+		case HikeConstants.PLATFORM_REQUEST:
+			mAdapter.onActivityResult(requestCode, resultCode, data);
+
 		}
 	}
 
@@ -1704,7 +1708,9 @@ public abstract class ChatThread extends SimpleOnGestureListener implements Over
 			}
 			if (message.getState() == ConvMessage.State.RECEIVED_UNREAD)
 			{
-				mConversationsView.setSelection(messages.size() - mConversation.getUnreadCount() - 1);
+				int index = (messages.size() - mConversation.getUnreadCount() - 1) ;
+				index = index >= 0 ? index : 0;
+				mConversationsView.setSelection(index);
 			}
 			else
 			{
