23b8081adfc7e9b58ce4ad2c7662518eb6be94fc
Shobhit <shobhit@Shobhits-MacBook-Pro.local>
2015-04-27 21:20:20 +0530

    api to get botInfo of bots

--- src/com/bsb/hike/utils/Utils.java
+++ src/com/bsb/hike/utils/Utils.java
@@ -43,6 +43,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.zip.GZIPInputStream;
 
+import com.bsb.hike.bots.BotInfo;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -5638,6 +5639,11 @@ public class Utils
 		}
 		
 		return maxVal;
+	}
+
+	public static BotInfo getBotInfoForBotMsisdn(String msisdn)
+	{
+		return HikeMessengerApp.hikeBotNamesMap.get(msisdn);
 	}
 	
 	public static boolean isOnProduction()
