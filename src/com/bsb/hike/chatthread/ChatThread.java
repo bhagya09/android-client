51ea4b4322367869ac50f56e86d8cd09bd4ab407
gauravKhanna <gaurav@bsb.in>
2015-04-29 15:39:31 +0530

    theme picker action mode orientation change bug solved fogbugz : 43766

--- src/com/bsb/hike/chatthread/ChatThread.java
+++ src/com/bsb/hike/chatthread/ChatThread.java
@@ -229,8 +229,6 @@ public abstract class ChatThread extends SimpleOnGestureListener implements Over
     
     private static final int SHARING_FUNCTIONALITY = 34;
     
-    private static final int THEME_PICKER_CONFIG_CHANGE = 35;
-
     private int NUDGE_TOAST_OCCURENCE = 2;
     	
     private int currentNudgeCount = 0;
@@ -449,13 +447,6 @@ public abstract class ChatThread extends SimpleOnGestureListener implements Over
 			break;
 		case MUTE_CONVERSATION_TOGGLED:
 			muteConvToggledUIChange((boolean) msg.obj);
-			break;
-		case THEME_PICKER_CONFIG_CHANGE : 
-			if (themePicker != null && themePicker.isShowing())
-			{
-				themePicker.setOrientation(activity.getResources().getConfiguration().orientation);
-				themePicker.refreshViews(true);
-			}
 			break;
 		default:
 			Logger.d(TAG, "Did not find any matching event for msg.what : " + msg.what);
@@ -4471,7 +4462,7 @@ public abstract class ChatThread extends SimpleOnGestureListener implements Over
 		
 		if (themePicker != null && themePicker.isShowing())
 		{
-			uiHandler.sendEmptyMessage(THEME_PICKER_CONFIG_CHANGE);
+			themePicker.onOrientationChange(newConfig.orientation);
 		}
 		
 		if (this.dialog != null)
