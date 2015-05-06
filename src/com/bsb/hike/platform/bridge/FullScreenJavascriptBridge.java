9ba9cc247347f68dbdc0138755655ad998d9b594
piyushghai <piyush@hike.in>
2015-05-06 12:16:11 +0530

    Initial commit for fullscreen javascript bridge

--- src/com/bsb/hike/platform/bridge/FullScreenJavascriptBridge.java
+++ src/com/bsb/hike/platform/bridge/FullScreenJavascriptBridge.java
@@ -4,15 +4,15 @@ import android.app.Activity;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 
-import com.bsb.hike.platform.bridge.JavascriptBridge;
-
 public class FullScreenJavascriptBridge extends JavascriptBridge
 {
+	
+	private String msisdn;
 
-	public FullScreenJavascriptBridge(WebView mWebView, Activity activity)
+	public FullScreenJavascriptBridge(WebView mWebView, Activity activity, String msisdn)
 	{
 		super(activity, mWebView);
-
+		this.msisdn = msisdn;
 	}
 
 	@Override
@@ -34,5 +34,32 @@ public class FullScreenJavascriptBridge extends JavascriptBridge
 	{
 		openActivity(metaData);
 	}
+
+	/**
+	 * this function will update the helper data. It will replace the key if it is present in the helper data and will add it if it is not present in the helper data.
+	 * 
+	 * @param json
+	 */
+	@JavascriptInterface
+	public void updateHelperData(String json)
+	{
+
+	}
+
+	/**
+	 * Calling this function will initiate forward of the message to a friend or group.
+	 * 
+	 * @param json
+	 *            : if the data has changed , then send the updated fields and it will update the metadata. If the key is already present, it will be replaced else it will be added
+	 *            to the existent metadata. If the json has JSONObject as key, there would be another round of iteration, and will replace the key-value pair if the key is already
+	 *            present and will add the key-value pair if the key is not present in the existent metadata.
+	 */
+	@JavascriptInterface
+	public void forwardToChat(String json)
+	{
+
+	}
+	
+	
 
 }
