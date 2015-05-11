deveshmittal/hike

migrating delete unlink account request

src/com/bsb/hike/modules/httpmgr/HttpRequestConstants.java
@@ -189,4 +189,14 @@ public class HttpRequestConstants
 	{
 		return BASE_URL + BASE_V1 + "/pa";
 	}
+
+	public static String deleteAccountBase()
+	{
+		return BASE_URL + BASE_V1 + BASE_ACCOUNT;
+	}
+
+	public static String unlinkAccountBase()
+	{
+		return BASE_URL + BASE_V1 + BASE_ACCOUNT + "/unlink";
+	}
 }

bhagya09/android-client

src/com/bsb/hike/modules/httpmgr/hikehttp/HttpRequestConstants.java
@@ -156,4 +156,14 @@ public class HttpRequestConstants
 	{
 		return BASE_URL + BASE_V1 + "/";
 	}
+	
+	public static String deleteAccountBaseUrl()
+	{
+		return BASE_URL + BASE_V1 + BASE_ACCOUNT;
+	}
+
+	public static String unlinkAccountBaseUrl()
+	{
+		return BASE_URL + BASE_V1 + BASE_ACCOUNT + "/unlink";
+	}
 }