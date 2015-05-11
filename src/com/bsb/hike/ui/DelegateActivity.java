c8d9afbb623af52c4777e0087f89e6ac7925c9a8
Akhil-Tripathi <akhiltripathi@Akhils-MacBook-Pro.local>
2015-05-11 12:51:40 +0530

    Formatting changes

--- src/com/bsb/hike/ui/DelegateActivity.java
+++ src/com/bsb/hike/ui/DelegateActivity.java
@@ -12,17 +12,14 @@ import com.bsb.hike.HikeMessengerApp;
 import com.bsb.hike.utils.HikeAppStateBaseFragmentActivity;
 import com.bsb.hike.utils.Logger;
 
-
-
 /**
- * The activity handles multiple activity intents.
- * Starts the Activities provided in the intents array sequentially  and if started for result returns the result of the final activity to the calling activity.
- * Use this when you want to start activity A and then use its result to start B and use its result to start C and so on...
+ * The activity handles multiple activity intents. Starts the Activities provided in the intents array sequentially and if started for result returns the result of the final
+ * activity to the calling activity. Use this when you want to start activity A and then use its result to start B and use its result to start C and so on...
  * 
  * If started for for result it will the result of the last activity < C in the explanation > to its calling activity otherwise will finish itself on starting the last activity <C>
  * 
  * Array of destination intents is must for this activity otherwise it will exit with Result code RESULT_CANCELED
- *  
+ * 
  */
 public class DelegateActivity extends HikeAppStateBaseFragmentActivity
 {
@@ -75,19 +72,10 @@ public class DelegateActivity extends HikeAppStateBaseFragmentActivity
 
 			destinationIntents.add((Intent) destParcel);
 		}
+		
+		Logger.d(TAG, "Starting activity for result");
+		DelegateActivity.this.startActivityForResult(destinationIntents.get(currentIntentCounter++), requestCode);
 
-		if (startedForResult)
-		{
-			Logger.d(TAG, "Starting activity for result");
-			DelegateActivity.this.startActivityForResult(destinationIntents.get(currentIntentCounter++), requestCode);
-
-		}
-		else
-		{
-			Logger.d(TAG, "Starting activity");
-			DelegateActivity.this.startActivity(destinationIntents.get(currentIntentCounter++));
-			DelegateActivity.this.finish();
-		}
 	}
 
 	private void onError()
@@ -103,7 +91,7 @@ public class DelegateActivity extends HikeAppStateBaseFragmentActivity
 
 		if (resultCode != RESULT_OK)
 		{
-			//Delegate activity should not handle error states.The activity starting it should handle it.
+			// Delegate activity should not handle error states.The activity starting it should handle it.
 			Logger.d(TAG, "Invalid RESULT CODE = " + resultCode);
 			onError();
 		}
@@ -119,15 +107,15 @@ public class DelegateActivity extends HikeAppStateBaseFragmentActivity
 
 		boolean receivedFromCamera = destinationIntents.get(currentIntentCounter - 1).getAction() == MediaStore.ACTION_IMAGE_CAPTURE;// Special case since camera returns null data
 		boolean isLastIntent = false;
-		
+
 		/**
-		 * Will determine the next intent to be picked from the intents array provided to be launched
-		 * If the next Intent is not the last intent of the destinations array it starts the next intent for result
-		 * If DelegateActivity was not started for result and its the last intent from the array, it opens the last intent and exits.
-		 * If DelegateActivity was started for result and its its the last intent from the array, it launches the last activity for result and returns its result to the activity that started DelegateActivity
+		 * Will determine the next intent to be picked from the intents array provided to be launched If the next Intent is not the last intent of the destinations array it starts
+		 * the next intent for result If DelegateActivity was not started for result and its the last intent from the array, it opens the last intent and exits. If DelegateActivity
+		 * was started for result and its its the last intent from the array, it launches the last activity for result and returns its result to the activity that started
+		 * DelegateActivity
 		 * 
 		 */
-		
+
 		if (startedForResult)
 		{
 			if (currentIntentCounter < destinationIntents.size())
@@ -149,7 +137,6 @@ public class DelegateActivity extends HikeAppStateBaseFragmentActivity
 			}
 		}
 
-		
 		if (receivedFromCamera)
 		{
 			// Special case since camera returns null data
@@ -161,7 +148,7 @@ public class DelegateActivity extends HikeAppStateBaseFragmentActivity
 		}
 		else if (data != null)
 		{
-			//transfers data received to the next intent
+			// transfers data received to the next intent
 			currentIntent.setAction(data.getAction());
 			currentIntent.setData(data.getData());
 			currentIntent.putExtras(data.getExtras());
@@ -169,15 +156,18 @@ public class DelegateActivity extends HikeAppStateBaseFragmentActivity
 
 		if (!isLastIntent)
 		{
+			Logger.d(TAG, "Starting activity for result");
 			DelegateActivity.this.startActivityForResult(currentIntent, this.requestCode);
 		}
 		else if (!startedForResult)
 		{
+			Logger.d(TAG, "Starting final activity");
 			DelegateActivity.this.startActivity(currentIntent);
 			DelegateActivity.this.finish();
 		}
 		else
 		{
+			Logger.d(TAG, "Returning result of final activity to caller");
 			DelegateActivity.this.setResult(RESULT_OK, currentIntent);
 			DelegateActivity.this.finish();
 		}
