2ae4ebba0ffdca9cd881c78df0d2c1d9552009ee
Anuj Jain <anuj@hike.in>
2015-05-13 13:32:41 +0530

    New ringtone audio attributes for devices >= lollipop

--- src/com/bsb/hike/utils/Utils.java
+++ src/com/bsb/hike/utils/Utils.java
@@ -3234,6 +3234,11 @@ public class Utils
 		return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
 	}
 	
+	public static boolean isLollipopOrHigher()
+	{
+		return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+	}
+	
 	public static boolean isJELLY_BEAN_MR2OrHigher()
 	{
 		return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2;
