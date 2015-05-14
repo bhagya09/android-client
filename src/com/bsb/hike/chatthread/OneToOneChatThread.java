156b237132f243c831cd5a446ce26bada8803647
Pankaj Verma <pankajv@hike.in>
2015-05-14 16:24:28 +0530

    We should not have 'not null' check here fixed bug https://hike.fogbugz.com/default.asp?44630

--- src/com/bsb/hike/chatthread/OneToOneChatThread.java
+++ src/com/bsb/hike/chatthread/OneToOneChatThread.java
@@ -1497,7 +1497,7 @@ public class OneToOneChatThread extends ChatThread implements LastSeenFetchedCal
 	 */
 	private void onAppForegrounded()
 	{
-		if (mContactInfo != null)
+		if (mContactInfo == null)
 		{
 			return;
 		}
