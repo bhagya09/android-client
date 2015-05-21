e9501df78dc7a0a198ac516e29400907c0390fd3
Himanshu Khandelwal <himanshu@hike.in>
2015-05-21 18:02:21 +0530

    Added OfflineChatThread class

---
+++ src/com/bsb/hike/chatthread/OfflineChatThread.java
@@ -0,0 +1,51 @@
+package com.bsb.hike.chatthread;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.actionbarsherlock.view.Menu;
+import com.bsb.hike.R;
+import com.bsb.hike.media.OverFlowMenuItem;
+import com.bsb.hike.models.Conversation.Conversation;
+
+public class OfflineChatThread extends OneToOneChatThread
+{
+
+	public OfflineChatThread(ChatThreadActivity activity, String msisdn)
+	{
+		super(activity, msisdn);
+	}
+
+	@Override
+	protected Conversation fetchConversation()
+	{
+		return super.fetchConversation();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+
+		if (mConversation != null)
+		{
+			mActionBar.onCreateOptionsMenu(menu, R.menu.one_one_chat_thread_menu, getOverFlowItems(), this, this);
+			return super.onCreateOptionsMenu(menu);
+		}
+		return false;
+	}
+	
+	private List<OverFlowMenuItem> getOverFlowItems()
+	{
+		List<OverFlowMenuItem> list = new ArrayList<OverFlowMenuItem>();
+		list.add(new OverFlowMenuItem(getString(R.string.view_profile), 0, 0, R.string.view_profile));
+		list.add(new OverFlowMenuItem(getString(R.string.chat_theme), 0, 0, R.string.chat_theme));
+		list.add(new OverFlowMenuItem(mConversation.isBlocked() ? getString(R.string.unblock_title) : getString(R.string.block_title), 0, 0, R.string.block_title));
+		
+		for (OverFlowMenuItem item : super.getOverFlowMenuItems())
+		{
+			list.add(item);
+		}
+		return list;
+	}
+	
+}
