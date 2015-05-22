41e983867cacc27bd2af89d58b75842580cbd3c3
Himanshu Khandelwal <himanshu@hike.in>
2015-05-22 12:57:30 +0530

    Adding a interface for  listening offline Callbacks

---
+++ src/com/bsb/hike/offline/IOfflineCallbacks.java
@@ -0,0 +1,18 @@
+package com.bsb.hike.offline;
+
+import java.util.HashMap;
+
+import android.net.wifi.ScanResult;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+
+public interface IOfflineCallbacks
+{
+	public void wifiP2PScanResults(WifiP2pDeviceList peerList);
+
+	public void wifiScanResults(HashMap<String, ScanResult> list);
+
+	public void onConnect();
+
+	public void onDisconnect();
+
+}
