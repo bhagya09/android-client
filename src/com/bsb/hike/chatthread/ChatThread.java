edd11d84afbcaf41fb2d0c565750fec8a06c2889
gaurav-m <gauravm@bsb.in>
2015-05-25 13:31:02 +0530

    added cap on number of messgaes to be loaded in one go

--- src/com/bsb/hike/chatthread/ChatThread.java
+++ src/com/bsb/hike/chatthread/ChatThread.java
@@ -2460,7 +2460,7 @@ public abstract class ChatThread extends SimpleOnGestureListener implements Over
 	
 	private static class MessageFinder extends AsyncTaskLoader<Object>
 	{
-		static long ADD_MSG_TIME = 2000;
+		static long ADD_MSG_TIME = 3000;
 
 		int loaderId;
 	
@@ -2517,7 +2517,11 @@ public abstract class ChatThread extends SimpleOnGestureListener implements Over
 						{
 							e.printStackTrace();
 						}
-						loadMessageCount *= 2;
+						//No need to load more than 3200 messaging in one go.
+						if (loadMessageCount < 3200)
+						{
+							loadMessageCount *= 2;
+						}
 						position = messageSearchManager.getPrevItem(msgList.size());
 					}
 					if (loaderId == SEARCH_LOOP && position < 0)
