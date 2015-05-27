582c9cd8d965773e095f6c82a168fc519f7fee2d
Himanshu Khandelwal <himanshu@hike.in>
2015-05-27 17:08:29 +0530

    Added constants file

---
+++ src/com/bsb/hike/offline/OfflineConstants.java
@@ -0,0 +1,22 @@
+package com.bsb.hike.offline;
+
+public class OfflineConstants
+{
+	public static  final int MAXTRIES = 1;
+	
+	public static final int PORT_PING = 18977;
+
+	public static final int PORT_FILE_TRANSFER = 18988;
+
+	public static final int PORT_TEXT_MESSAGE = 18999;
+
+	public static final int SOCKET_TIMEOUT = 5000;
+
+	public static final int pingTimeout = 60 * 1000;
+	
+	public static final String SUBNET = "192.168.43.";
+
+	public static final String IP_SERVER = "192.168.43.1";
+	
+	public static final int CHUNK_SIZE=1024;
+}
