d45de64d9ae66f276b05a6eac202b2458c226215
Himanshu Khandelwal <himanshu@hike.in>
2015-05-27 17:07:37 +0530

    Adding a manager class to manage functionalities

--- src/com/bsb/hike/offline/OfflineManager.java
+++ src/com/bsb/hike/offline/OfflineManager.java
@@ -2,5 +2,46 @@ package com.bsb.hike.offline;
 
 public class OfflineManager
 {
-	public static final OfflineManager _instance=new  OfflineManager();
+	public static final OfflineManager _instance = new OfflineManager();
+
+	private boolean isHotSpotCreated = false;
+
+	private OfflineManager()
+	{
+		init();
+	}
+
+	public static OfflineManager getInstance()
+	{
+		return _instance;
+	}
+	
+	/**
+	 * Initialize all your functions here
+	 */
+	private void init()
+	{
+
+	}
+
+	public void setIsHotSpotCreated(boolean isHotSpotCreated)
+	{
+		this.isHotSpotCreated = isHotSpotCreated;
+	}
+
+	public boolean isHotspotCreated()
+	{
+		return isHotSpotCreated;
+	}
+	
+	public void addToTextQueue()
+	{
+		
+	}
+	
+	public void addToFileQueue()
+	{
+		
+	}
+	
 }
