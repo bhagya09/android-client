f007b2ae37fd3ee5111fbe5ae1e5e3dc1fed3cb2
Akhil-Tripathi <akhiltripathi@Akhils-MacBook-Pro.local>
2015-05-31 21:27:41 +0530

    Removing Unused Code

--- src/com/bsb/hike/ui/HomeActivity.java
+++ src/com/bsb/hike/ui/HomeActivity.java
@@ -1897,12 +1897,6 @@ public class HomeActivity extends HikeAppStateBaseFragmentActivity implements Li
 					// Take a photo
 					accountPrefs.edit().putBoolean(HikeConstants.SHOW_PHOTOS_RED_DOT, false).commit();
 					
-					String destString = Utils.getFileParent(HikeFileType.IMAGE, false)+ File.separator + Utils.getOriginalFile(HikeFileType.IMAGE, null);
-					
-					Intent picEditerIntent = IntentFactory.getPictureEditorActivityIntent(HomeActivity.this, null, true, destString,false);
-					
-					PendingIntent editorPendingIntent = PendingIntent.getActivity(HikeMessengerApp.getInstance().getApplicationContext(), RESULT_OK, picEditerIntent,PendingIntent.FLAG_UPDATE_CURRENT);
-					
 					// Open gallery
 					int galleryFlags = GalleryActivity.GALLERY_CATEGORIZE_BY_FOLDERS|GalleryActivity.GALLERY_EDIT_SELECTED_IMAGE|GalleryActivity.GALLERY_COMPRESS_EDITED_IMAGE|GalleryActivity.GALLERY_DISPLAY_CAMERA_ITEM;
 					Intent galleryPickerIntent = IntentFactory.getHikeGalleryPickerIntent(HomeActivity.this, galleryFlags,null);
