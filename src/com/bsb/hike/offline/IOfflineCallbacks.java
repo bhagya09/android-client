d331ecfe797cfbd22ae124b96b7d5c9103dcb81d
Himanshu Khandelwal <himanshu@hike.in>
2015-06-04 17:03:38 +0530

    adding another function

--- src/com/bsb/hike/offline/IOfflineCallbacks.java
+++ src/com/bsb/hike/offline/IOfflineCallbacks.java
@@ -2,6 +2,8 @@ package com.bsb.hike.offline;
 
 import java.util.Map;
 
+import com.bsb.hike.offline.OfflineConstants.ERRORCODE;
+
 import android.net.wifi.ScanResult;
 import android.net.wifi.p2p.WifiP2pDeviceList;
 
@@ -13,7 +15,7 @@ public interface IOfflineCallbacks
 
 	public void onConnect();
 
-	public void onDisconnect();
+	public void onDisconnect(ERRORCODE errorCode);
 
 	public void connectedToMsisdn(String connectedDevice);
 
