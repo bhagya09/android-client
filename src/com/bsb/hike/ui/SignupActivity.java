047bee0af9ebc3952e2ad3f0f41a7412a053a374
Pankaj Verma <pankajv@hike.in>
2015-06-12 10:48:03 +0530

    While reseting view flipper we don't need to go back to number screen

--- src/com/bsb/hike/ui/SignupActivity.java
+++ src/com/bsb/hike/ui/SignupActivity.java
@@ -1755,10 +1755,6 @@ public class SignupActivity extends ChangeProfileImageBaseActivity implements Si
 		errorDialog = null;
 		toggleActionBarElementsEnable(true);
 		viewFlipper.setVisibility(View.VISIBLE);
-		removeAnimation();
-		viewFlipper.setDisplayedChild(NUMBER);
-		prepareLayoutForFetchingNumber();
-		setAnimation();
 	}
 
 	private void restartTask()
