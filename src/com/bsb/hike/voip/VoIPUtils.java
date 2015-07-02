53a385caeb0e352cb30fcf693cdb7eded33470e5
Anuj Jain <anuj@hike.in>
2015-07-02 13:55:57 +0530

    Conferencing enabled.

--- src/com/bsb/hike/voip/VoIPUtils.java
+++ src/com/bsb/hike/voip/VoIPUtils.java
@@ -376,7 +376,7 @@ public class VoIPUtils {
 	
 	public static boolean isConferencingEnabled(Context context) 
 	{
-		boolean conferenceEnabled = HikeSharedPreferenceUtil.getInstance().getData(HikeConstants.VOIP_CONFERENCING_ENABLED, false);
+		boolean conferenceEnabled = HikeSharedPreferenceUtil.getInstance().getData(HikeConstants.VOIP_CONFERENCING_ENABLED, true);
 		return conferenceEnabled;
 	}
 	
