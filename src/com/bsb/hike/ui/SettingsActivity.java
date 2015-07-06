d8852240e53ec1390fb195a0facd346b75eeff80
Rohit Singhal <rohits@hike.in>
2015-07-06 15:26:11 +0530

    BUg Fix 913, 920 :- Apply compression on all DP flow + Same Image Size in History

--- src/com/bsb/hike/ui/SettingsActivity.java
+++ src/com/bsb/hike/ui/SettingsActivity.java
@@ -606,8 +606,6 @@ public class SettingsActivity extends ChangeProfileImageBaseActivity implements 
 	public String profileImageCropped()
 	{
 		String path = super.profileImageCropped();
-		int imageCompressQuality = HikeSharedPreferenceUtil.getInstance().getData(HikeConstants.SERVER_CONFIG_DEFAULT_IMAGE_SAVE_QUALITY, HikeConstants.HikePhotos.DEFAULT_IMAGE_SAVE_QUALITY);
-		Utils.compressAndCopyImage(path, path, SettingsActivity.this, Bitmap.Config.RGB_565, imageCompressQuality, ImageQuality.QUALITY_MEDIUM, false);
 		uploadProfilePicture(AccountUtils.USER_DP_UPDATE_URL);
 		return path;
 	}
