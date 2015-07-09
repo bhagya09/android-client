917d45149795c6df9c928b1c0e28188e248d161f
Shobhit <shobhit@Shobhits-MacBook-Pro.local>
2015-07-09 17:25:23 +0530

    zoom enabled in webView

--- src/com/bsb/hike/platform/CustomWebView.java
+++ src/com/bsb/hike/platform/CustomWebView.java
@@ -142,6 +142,8 @@ public class CustomWebView extends WebView
 		setVerticalScrollBarEnabled(false);
 		setHorizontalScrollBarEnabled(false);
 		getSettings().setJavaScriptEnabled(true);
+		getSettings().setSupportZoom(true);
+		getSettings().setBuiltInZoomControls(true);
 	}
 	
 	public void loadMicroAppData(String data)
