574a83ce688f05e701f0530cc8129e8c44a0e475
Rajesh <rajesh@bsb.in>
2015-07-14 15:30:17 +0530

    Disable logs.

--- src/com/bsb/hike/AppConfig.java
+++ src/com/bsb/hike/AppConfig.java
@@ -8,11 +8,11 @@ public class AppConfig
 	 * to create logs build for users 1. SHOW_LOGS = true; 2. SHOW_SEND_LOGS_OPTION = true 3. PRODUCTION_BROKER_HOST_NAME = "dmqtt.im.hike.in";
 	 */
 
-	public static boolean DEBUG_LOGS_ENABLED = Utils.isSendLogsEnabled() || BuildConfig.DEBUG;
+	public static boolean DEBUG_LOGS_ENABLED = false;// Utils.isSendLogsEnabled() || BuildConfig.DEBUG;
 
 	public static boolean SHOW_LOGS = DEBUG_LOGS_ENABLED;
 
-	public static boolean ALLOW_STAGING_TOGGLE = BuildConfig.DEBUG;
+	public static boolean ALLOW_STAGING_TOGGLE = false; //BuildConfig.DEBUG;
 
 	public static boolean SHOW_SEND_LOGS_OPTION = DEBUG_LOGS_ENABLED;
 
