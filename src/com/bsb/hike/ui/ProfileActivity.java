eacfc6f4aa60879b39cf8bd8d20f9949cf68df72
Himanshu Khandelwal <himanshu@hike.in>
2015-07-15 17:43:07 +0530

    removing check fr offline COnv

--- src/com/bsb/hike/ui/ProfileActivity.java
+++ src/com/bsb/hike/ui/ProfileActivity.java
@@ -645,10 +645,7 @@ public class ProfileActivity extends ChangeProfileImageBaseActivity implements F
 	private void setupContactProfileScreen()
 	{
 		setLocalMsisdn(getIntent().getStringExtra(HikeConstants.Extras.CONTACT_INFO));
-		if (Utils.isOfflineConversation(mLocalMSISDN))
-			contactInfo = ContactManager.getInstance().getContact(mLocalMSISDN.replace("o:", ""), true, true);
-		else
-			contactInfo = ContactManager.getInstance().getContact(mLocalMSISDN, true, true);
+		contactInfo = ContactManager.getInstance().getContact(mLocalMSISDN, true, true);
 		sharedMediaCount = HikeConversationsDatabase.getInstance().getSharedMediaCount(mLocalMSISDN, true);
 		sharedPinCount = 0;  //Add a query here to get shared groups count. sharedPincount is to be treated as shared group count here.
 		unreadPinCount = 0;
@@ -672,10 +669,7 @@ public class ProfileActivity extends ChangeProfileImageBaseActivity implements F
 	private void setupContactTimelineScreen()
 	{
 		setLocalMsisdn(getIntent().getStringExtra(HikeConstants.Extras.CONTACT_INFO_TIMELINE));
-		if (Utils.isOfflineConversation(mLocalMSISDN))
-			contactInfo = ContactManager.getInstance().getContact(mLocalMSISDN.replace("o:", ""), true, true);
-		else
-			contactInfo = ContactManager.getInstance().getContact(mLocalMSISDN, true, true);
+		contactInfo = ContactManager.getInstance().getContact(mLocalMSISDN, true, true);
 		if(!contactInfo.isOnhike())
 		{
 			contactInfo.setOnhike(getIntent().getBooleanExtra(HikeConstants.Extras.ON_HIKE, false));
