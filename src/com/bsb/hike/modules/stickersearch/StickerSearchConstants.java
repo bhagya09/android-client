77fcc33af17a946642976af0a48ff744763df84c
Anubhav <anubhav.7nov@gmail.com>
2015-07-16 20:40:57 +0530

    added constant

--- src/com/bsb/hike/modules/stickersearch/StickerSearchConstants.java
+++ src/com/bsb/hike/modules/stickersearch/StickerSearchConstants.java
@@ -13,4 +13,6 @@ public class StickerSearchConstants
 	public static final int STICKER_DATA_UPDATE_TRIAL = 1;
 	
 	public static final int REBALACING_DEFAULT_TIME = 4; //4 am
+	
+	public static final long DEFAULT_STICKER_TAG_REFRESH_TIME = 7 * 24 * 60 * 60 * 1000l; //7 days
 }
