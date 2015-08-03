f359bfdbf78afd0ff2360e74e21c8522c04713d9
Atul Mittal <atul@hike.in>
2015-08-03 18:20:47 +0530

    material timeline overflow menu

--- src/com/bsb/hike/timeline/view/TimelineActivity.java
+++ src/com/bsb/hike/timeline/view/TimelineActivity.java
@@ -1,8 +1,12 @@
 package com.bsb.hike.timeline.view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -10,12 +14,21 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.PopupMenu;
+import android.view.ViewGroup;
+import android.view.WindowManager.BadTokenException;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.LinearLayout.LayoutParams;
 
 import com.bsb.hike.HikeConstants;
 import com.bsb.hike.HikeMessengerApp;
@@ -29,6 +42,7 @@ import com.bsb.hike.db.HikeConversationsDatabase;
 import com.bsb.hike.dialog.HikeDialog;
 import com.bsb.hike.dialog.HikeDialogFactory;
 import com.bsb.hike.dialog.HikeDialogListener;
+import com.bsb.hike.media.OverFlowMenuItem;
 import com.bsb.hike.models.HikeHandlerUtil;
 import com.bsb.hike.productpopup.ProductPopupsConstants;
 import com.bsb.hike.ui.PeopleActivity;
@@ -45,21 +59,22 @@ public class TimelineActivity extends HikeAppStateBaseFragmentActivity implement
 	private Handler mHandler = new Handler();
 
 	private TextView overflowIndicator;
-	
-	//private TextView activityFeedTopBarIndicator;
+
+	// private TextView activityFeedTopBarIndicator;
 	private MenuItem activityFeedMenuItem;
-	
+
 	private SharedPreferences accountPrefs;
-	
+
 	private int unreadCounter = -1;
-	
+
 	public static final int NO_FEED_PRESENT = -1;
-	
+
 	public static final int FETCH_FEED_FROM_DB = -2;
 
 	private String[] homePubSubListeners = { HikePubSub.FAVORITE_COUNT_CHANGED, HikePubSub.ACTIVITY_FEED_COUNT_CHANGED };
 
 	private final String FRAGMENT_ACTIVITY_FEED_TAG = "fragmentActivityFeedTag";
+
 	@Override
 	public void onEventReceived(String type, Object object)
 	{
@@ -102,7 +117,7 @@ public class TimelineActivity extends HikeAppStateBaseFragmentActivity implement
 		{
 			loadActivityFeedFragment();
 		}
-		
+
 		FetchUnreadFeedsTask fetchUnreadFeedsTask = new FetchUnreadFeedsTask();
 
 		if (Utils.isHoneycombOrHigher())
@@ -179,13 +194,14 @@ public class TimelineActivity extends HikeAppStateBaseFragmentActivity implement
 			@Override
 			public void onClick(View v)
 			{
-				showTimelineMenuPopup(overflowMenuItem);
+//				showTimelineMenuPopup(overflowMenuItem);
+				showTimelineOverFlow();
 			}
 		});
-		
+
 		activityFeedMenuItem.getActionView().setOnClickListener(new View.OnClickListener()
 		{
-			
+
 			@Override
 			public void onClick(View v)
 			{
@@ -194,20 +210,42 @@ public class TimelineActivity extends HikeAppStateBaseFragmentActivity implement
 		});
 		return super.onCreateOptionsMenu(menu);
 	}
-	
-	public void showTimelineMenuPopup(View v)
+
+	private void showTimelineOverFlow()
 	{
-		PopupMenu popup = new PopupMenu(TimelineActivity.this, v);
-		android.view.MenuInflater inflater = popup.getMenuInflater();
-		inflater.inflate(R.menu.timeline_overflow_menu, popup.getMenu());
-		popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener()
+		final ArrayList<OverFlowMenuItem> optionsList = new ArrayList<OverFlowMenuItem>();
+
+		optionsList.add(new OverFlowMenuItem(getString(R.string.clear_timeline), 0, 0, R.string.clear_timeline));
+
+		optionsList.add(new OverFlowMenuItem(getString(R.string.favourites), 0, 0, R.string.favourites));
+
+		optionsList.add(new OverFlowMenuItem(getString(R.string.my_profile), 0, 0, R.string.my_profile));
+
+		final PopupWindow overFlowWindow = new PopupWindow(this);
+
+		FrameLayout homeScreen = (FrameLayout) findViewById(R.id.home_screen);
+
+		View parentView = getLayoutInflater().inflate(R.layout.overflow_menu, homeScreen, false);
+
+		overFlowWindow.setContentView(parentView);
+
+		ListView overFlowListView = (ListView) parentView.findViewById(R.id.overflow_menu_list);
+
+		TimelineOverflowAdapter overflowAdapter = new TimelineOverflowAdapter(this, R.layout.over_flow_menu_item, R.id.item_title, optionsList);
+
+		overFlowListView.setAdapter(overflowAdapter);
+
+		overFlowListView.setOnItemClickListener(new OnItemClickListener()
 		{
 			@Override
-			public boolean onMenuItemClick(android.view.MenuItem arg0)
+			public void onItemClick(AdapterView<?> adapterView, View view, int position, long id)
 			{
-				switch (arg0.getItemId())
+				overFlowWindow.dismiss();
+				OverFlowMenuItem item = (OverFlowMenuItem) adapterView.getItemAtPosition(position);
+
+				switch (item.id)
 				{
-				case R.id.clear_timeline:
+				case R.string.clear_timeline:
 
 					HikeDialogFactory.showDialog(TimelineActivity.this, HikeDialogFactory.WIPE_TIMELINE_DIALOG, new HikeDialogListener()
 					{
@@ -246,23 +284,72 @@ public class TimelineActivity extends HikeAppStateBaseFragmentActivity implement
 					});
 
 					break;
-				case R.id.favourites:
+				case R.string.favourites:
 					Intent intent = new Intent(TimelineActivity.this, PeopleActivity.class);
 					intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 					startActivity(intent);
 					break;
-				case R.id.my_profile:
+				case R.string.my_profile:
 					Intent intent2 = new Intent(TimelineActivity.this, ProfileActivity.class);
 					intent2.putExtra(HikeConstants.Extras.FROM_CENTRAL_TIMELINE, true);
 					startActivity(intent2);
 					break;
-				default:
-					break;
 				}
-				return false;
 			}
 		});
-		popup.show();
+
+		overFlowWindow.setBackgroundDrawable(getResources().getDrawable(android.R.color.transparent));
+		overFlowWindow.setOutsideTouchable(true);
+		overFlowWindow.setFocusable(true);
+		overFlowWindow.setWidth(getResources().getDimensionPixelSize(R.dimen.overflow_menu_width));
+		overFlowWindow.setHeight(LayoutParams.WRAP_CONTENT);
+		/*
+		 * In some devices Activity crashes and a BadTokenException is thrown by showAsDropDown method. Still need to find out exact repro of the bug.
+		 */
+		try
+		{
+			int rightMargin = getResources().getDimensionPixelSize(R.dimen.overflow_menu_right_margin);
+			overFlowWindow.showAsDropDown(findViewById(R.id.overflow_anchor), -rightMargin, 0);
+		}
+		catch (BadTokenException e)
+		{
+			Logger.e(getClass().getSimpleName(), "Excepetion in HomeActivity Overflow popup", e);
+		}
+		overFlowWindow.getContentView().setFocusableInTouchMode(true);
+		overFlowWindow.getContentView().setOnKeyListener(new View.OnKeyListener()
+		{
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event)
+			{
+				return onKeyUp(keyCode, event);
+			}
+		});
+
+	}
+
+	public class TimelineOverflowAdapter extends ArrayAdapter<OverFlowMenuItem>
+	{
+
+		public TimelineOverflowAdapter(Context context, int resource, int textViewResourceId, List<OverFlowMenuItem> objects)
+		{
+			super(context, resource, textViewResourceId, objects);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent)
+		{
+			if (convertView == null)
+			{
+				convertView = getLayoutInflater().inflate(R.layout.over_flow_menu_item, parent, false);
+			}
+
+			OverFlowMenuItem item = getItem(position);
+
+			TextView itemTextView = (TextView) convertView.findViewById(R.id.item_title);
+			itemTextView.setText(item.text);
+
+			return convertView;
+		}
 	}
 
 	@Override
@@ -313,7 +400,8 @@ public class TimelineActivity extends HikeAppStateBaseFragmentActivity implement
 	public void onBackPressed()
 	{
 		Fragment fragment = getSupportFragmentManager().findFragmentByTag(HikeConstants.IMAGE_FRAGMENT_TAG);
-		if (!(fragment != null && fragment.isVisible()) && (getIntent().getBooleanExtra(HikeConstants.Extras.FROM_NOTIFICATION, false) || getIntent().getBooleanExtra(HikeConstants.HikePhotos.HOME_ON_BACK_PRESS, false)))
+		if (!(fragment != null && fragment.isVisible())
+				&& (getIntent().getBooleanExtra(HikeConstants.Extras.FROM_NOTIFICATION, false) || getIntent().getBooleanExtra(HikeConstants.HikePhotos.HOME_ON_BACK_PRESS, false)))
 		{
 			IntentFactory.openHomeActivity(TimelineActivity.this, true);
 		}
@@ -345,7 +433,7 @@ public class TimelineActivity extends HikeAppStateBaseFragmentActivity implement
 				});
 			}
 		}
-		
+
 	}
 
 	@Override
@@ -415,18 +503,18 @@ public class TimelineActivity extends HikeAppStateBaseFragmentActivity implement
 				final TextView activityFeedTopBarIndicator = (TextView) activityFeedMenuItem.getActionView().findViewById(R.id.top_bar_indicator_text);
 				if (activityFeedTopBarIndicator != null)
 				{
-					if(count ==  NO_FEED_PRESENT)
+					if (count == NO_FEED_PRESENT)
 					{
 						activityFeedMenuItem.setVisible(false);
 					}
 					else
 					{
 						activityFeedMenuItem.setVisible(true);
-						if(count == 0)
+						if (count == 0)
 						{
 							activityFeedTopBarIndicator.setVisibility(View.GONE);
 						}
-						else 
+						else
 						{
 							if (count > 9)
 							{
@@ -451,29 +539,26 @@ public class TimelineActivity extends HikeAppStateBaseFragmentActivity implement
 		/*
 		 * Making sure we don't add the fragment if the activity is finishing.
 		 */
-		if (isFinishing()) {
+		if (isFinishing())
+		{
 			return;
 		}
 	}
-	
+
 	private void loadActivityFeedFragment()
 	{
 		ActivityFeedFragment activityFeedFragment = new ActivityFeedFragment();
-		getSupportFragmentManager().
-		beginTransaction().
-		add(R.id.parent_layout, activityFeedFragment, FRAGMENT_ACTIVITY_FEED_TAG).
-		addToBackStack(null).
-		commit();
+		getSupportFragmentManager().beginTransaction().add(R.id.parent_layout, activityFeedFragment, FRAGMENT_ACTIVITY_FEED_TAG).addToBackStack(null).commit();
 
 	}
-	
+
 	class FetchUnreadFeedsTask extends AsyncTask<Void, Void, Integer>
 	{
 
 		@Override
 		protected Integer doInBackground(Void... params)
 		{
-			if(HikeConversationsDatabase.getInstance().isAnyFeedEntryPresent())
+			if (HikeConversationsDatabase.getInstance().isAnyFeedEntryPresent())
 			{
 				return HikeConversationsDatabase.getInstance().getUnreadActivityFeedCount();
 			}
@@ -482,12 +567,12 @@ public class TimelineActivity extends HikeAppStateBaseFragmentActivity implement
 				return NO_FEED_PRESENT;
 			}
 		}
-		
+
 		@Override
 		protected void onPostExecute(Integer result)
 		{
 			unreadCounter = result;
 		}
-		
+
 	}
 }
