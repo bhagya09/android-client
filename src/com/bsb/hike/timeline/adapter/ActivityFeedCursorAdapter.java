41f64115d47dcaad2c3c0cd7a15c1d40df6670c2
Atul Mittal <atul@hike.in>
2015-08-07 15:04:30 +0530

    bug fix

--- src/com/bsb/hike/timeline/adapter/ActivityFeedCursorAdapter.java
+++ src/com/bsb/hike/timeline/adapter/ActivityFeedCursorAdapter.java
@@ -162,7 +162,11 @@ public class ActivityFeedCursorAdapter extends RecyclerViewCursorAdapter<Activit
 
 			if (feedDataModel.getReadStatus() == 1)
 			{
-				viewHolder.loveStatus.setImageResource(R.drawable.ic_loved);
+				viewHolder.loveStatus.setVisibility(View.GONE);
+			}
+			else
+			{
+				viewHolder.loveStatus.setVisibility(View.VISIBLE);
 			}
 
 			if (statusMessage.hasMood())
@@ -186,11 +190,11 @@ public class ActivityFeedCursorAdapter extends RecyclerViewCursorAdapter<Activit
 			}
 			else if (viewType == IMAGE || viewType == TEXT_IMAGE)
 			{
-				viewHolder.mainInfo.setText(mContext.getString(R.string.photo_like_text) +" " + Utils.getFormattedTime(true, mContext, feedDataModel.getTimestamp()));
+				viewHolder.mainInfo.setText(mContext.getString(R.string.photo_like_text) +". " + Utils.getFormattedTime(true, mContext, feedDataModel.getTimestamp()));
 			}
 			else
 			{
-				viewHolder.mainInfo.setText(mContext.getString(R.string.dp_like_text) +" " + Utils.getFormattedTime(true, mContext, feedDataModel.getTimestamp()));
+				viewHolder.mainInfo.setText(mContext.getString(R.string.dp_like_text) +". " + Utils.getFormattedTime(true, mContext, feedDataModel.getTimestamp()));
 			}
 
 			ImageViewerInfo imageViewerInfo = new ImageViewerInfo(statusMessage.getMappedId(), null, true);
