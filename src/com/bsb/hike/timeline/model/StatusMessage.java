12d41cf8b23a6f6a4926293ce82fa1356ac3dd7b
Atul Mittal <atul@hike.in>
2015-08-07 10:06:09 +0530

    bug fix https://hikeapp.atlassian.net/browse/AND-1712

--- src/com/bsb/hike/timeline/model/StatusMessage.java
+++ src/com/bsb/hike/timeline/model/StatusMessage.java
@@ -130,7 +130,7 @@ public class StatusMessage
 			this.fileKey = data.optString(HikeConstants.SU_IMAGE_KEY);
 			this.statusMessageType = StatusMessageType.IMAGE;
 		}
-		this.moodId = data.optInt(HikeConstants.MOOD);
+		this.moodId = data.optInt(HikeConstants.MOOD) - 1;
 		this.timeOfDay = data.optInt(HikeConstants.TIME_OF_DAY);
 		
 		if(data.has(HikeConstants.HISTORICAL_UPDATE))
