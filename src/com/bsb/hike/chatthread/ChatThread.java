71cbfab64a35915b49c20548c0eec21eff54863e
Anu Bansal <anubansal@Anus-MacBook-Pro.local>
2015-08-19 18:42:40 +0530

    Added globeKey language picker functionality

--- src/com/bsb/hike/chatthread/ChatThread.java
+++ src/com/bsb/hike/chatthread/ChatThread.java
@@ -16,71 +16,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.Activity;
-import android.app.NotificationManager;
-import android.app.ProgressDialog;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.SharedPreferences.Editor;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
-import android.media.AudioManager;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.preference.PreferenceManager;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.LoaderManager.LoaderCallbacks;
-import android.support.v4.content.AsyncTaskLoader;
-import android.support.v4.content.Loader;
-import android.support.v4.content.LocalBroadcastManager;
-import android.support.v4.view.MenuItemCompat;
-import android.text.Editable;
-import android.text.InputType;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.TextUtils;
-import android.text.TextWatcher;
-import android.text.style.StyleSpan;
-import android.util.Pair;
-import android.view.GestureDetector;
-import android.view.GestureDetector.SimpleOnGestureListener;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnKeyListener;
-import android.view.View.OnTouchListener;
-import android.view.ViewGroup.LayoutParams;
-import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.AbsListView;
-import android.widget.AbsListView.OnScrollListener;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemLongClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.ImageView.ScaleType;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.PopupWindow;
-import android.widget.PopupWindow.OnDismissListener;
-import android.widget.TextView;
-import android.widget.TextView.OnEditorActionListener;
-import android.widget.Toast;
-
 import com.bsb.hike.HikeConstants;
 import com.bsb.hike.HikeConstants.MESSAGE_TYPE;
 import com.bsb.hike.HikeMessengerApp;
@@ -165,10 +100,142 @@ import com.bsb.hike.view.CustomFontEditText.BackKeyListener;
 import com.bsb.hike.view.CustomLinearLayout;
 import com.bsb.hike.view.CustomLinearLayout.OnSoftKeyboardListener;
 import com.kpt.adaptxt.beta.CustomKeyboard;
+import com.kpt.adaptxt.beta.GlobeKeyData;
 import com.kpt.adaptxt.beta.util.KPTConstants;
 import com.kpt.adaptxt.beta.view.AdaptxtEditText;
 import com.kpt.adaptxt.beta.view.AdaptxtEditText.AdaptxtEditTextEventListner;
 import com.kpt.adaptxt.beta.view.AdaptxtEditText.AdaptxtKeyboordVisibilityStatusListner;
+
+import android.app.Activity;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.NotificationManager;
+import android.app.NotificationManager;
+import android.app.ProgressDialog;
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentFilter;
+import android.content.SharedPreferences.Editor;
+import android.content.SharedPreferences.Editor;
+import android.content.res.Configuration;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Drawable;
+import android.media.AudioManager;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Message;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.content.Loader;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.view.MenuItemCompat;
+import android.text.Editable;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.InputType;
+import android.text.Spannable;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableString;
+import android.text.TextUtils;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.text.TextWatcher;
+import android.text.style.StyleSpan;
+import android.text.style.StyleSpan;
+import android.util.Pair;
+import android.util.Pair;
+import android.view.ContextThemeWrapper;
+import android.view.GestureDetector;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.KeyEvent;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.view.View.OnKeyListener;
+import android.view.View.OnTouchListener;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup.LayoutParams;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.AnimationUtils;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AbsListView;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.Button;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.PopupWindow;
+import android.widget.PopupWindow.OnDismissListener;
+import android.widget.PopupWindow.OnDismissListener;
+import android.widget.TextView;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+import android.widget.TextView.OnEditorActionListener;
+import android.widget.Toast;
+import android.widget.Toast;
 
 /**
  * 
@@ -301,7 +368,12 @@ public abstract class ChatThread extends SimpleOnGestureListener implements Over
 	
 	private volatile boolean _doubleTapPref = false;
 
+	private ArrayList<String> unsupportedLanguage;
 
+	private String[] supportedLanguages;
+	
+	private int currentLanguageIndex;
+	
 	private int currentFirstVisibleItem = Integer.MAX_VALUE;
 
 	protected boolean loadingMoreMessages;
@@ -5975,10 +6047,104 @@ public abstract class ChatThread extends SimpleOnGestureListener implements Over
 	@Override
 	public void showGlobeKeyView()
 	{
-		// TODO Auto-generated method stub
-		
+		GlobeKeyData globeKeyData = mCustomKeyboard.getGlobeKeydata();
+
+		if (null != globeKeyData)
+		{
+
+			final int status = globeKeyData.getStatus();
+			if (status == GlobeKeyData.STATUS_OK)
+			{
+				currentLanguageIndex = globeKeyData.getCurrentIndex();
+				Logger.e("KPT", " current index " + currentLanguageIndex);
+				supportedLanguages = globeKeyData.getDisplayLanguages();
+
+				if (null != supportedLanguages)
+				{
+					for (int i = 0; i < supportedLanguages.length; i++)
+					{
+						Logger.e("KPT", " Language name " + supportedLanguages[i]);
+					}
+
+				}
+				else
+				{
+					Logger.e("KPT", " Strange KPT failed to provide display language name ");
+				}
+
+				unsupportedLanguage = globeKeyData.getUnsupportedLangugeList();
+				final int size = unsupportedLanguage.size();
+
+				if (size > 0)
+				{
+
+					for (String string : unsupportedLanguage)
+					{
+						Logger.e("KPT", " device unsupported " + string);
+					}
+				}
+				else
+				{
+					Logger.e("KPT", " GOOD! Device support all the languag of KPT ");
+				}
+
+				createBuilder(globeKeyData);
+
+				// We need to inform this to hike // 1 is the selected language index
+				/*
+				 * mCustomKeyboard.processChangeLanguageForDialog(1); AdaptxtEditText editText1 = (AdaptxtEditText)findViewById(R.id.edittext1);
+				 * mCustomKeyboard.showCustomKeyboard(editText1, true);
+				 */
+			}
+		}
+		else
+		{
+			Logger.e("KPT", " KPT failed to provide globe key data ");
+		}
 	}
 
+	private void createBuilder(final GlobeKeyData globeKeyData)
+	{
+		ContextThemeWrapper mContextWrapper = new ContextThemeWrapper(activity, R.style.AdaptxtTheme);
+		AlertDialog.Builder builderNew = new AlertDialog.Builder(mContextWrapper);
+		
+		builderNew.setCancelable(true);
+		// builder.setIcon(R.drawable.adaptxt_launcher_icon);
+		builderNew.setNegativeButton(android.R.string.cancel, null);
+		builderNew.setTitle("Keyboard Language");
+		builderNew.setSingleChoiceItems(supportedLanguages, -1, new DialogInterface.OnClickListener()
+		{
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which)
+			{
+				currentLanguageIndex = which;
+				mCustomKeyboard.processChangeLanguageForDialog(which);
+				dialog.dismiss();
+			}
+		});
+		final int color;
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB)
+		{
+			color = activity.getResources().getColor(R.color.kpt_balck_color_text);
+		}
+		else
+		{
+			color = activity.getResources().getColor(R.color.kpt_white_color_text);
+		}
+
+		AlertDialog mOptionsDialog = builderNew.create();
+
+		Window window = mOptionsDialog.getWindow();
+		WindowManager.LayoutParams lp = window.getAttributes();
+		lp.token = activity.getWindow().getDecorView().getRootView().getWindowToken();
+		lp.type = WindowManager.LayoutParams.TYPE_APPLICATION_ATTACHED_DIALOG;
+		window.setAttributes(lp);
+		window.addFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+
+		mOptionsDialog.show();
+	}
+	
 	@Override
 	public void showQuickSettingView()
 	{
