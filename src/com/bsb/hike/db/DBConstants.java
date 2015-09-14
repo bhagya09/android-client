ea26942bf370300ddfe12f9120f01dcc8731a554
piyushghai <piyush@hike.in>
2015-09-14 21:03:04 +0530

    Db version changed to 45

--- src/com/bsb/hike/db/DBConstants.java
+++ src/com/bsb/hike/db/DBConstants.java
@@ -4,7 +4,7 @@ import com.bsb.hike.models.ContactInfo.FavoriteType;
 
 public interface DBConstants
 {
-	public static final int CONVERSATIONS_DATABASE_VERSION = 44;
+	public static final int CONVERSATIONS_DATABASE_VERSION = 45;
 
 	public static final int USERS_DATABASE_VERSION = 17;
 
