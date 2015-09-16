16fb0a1174689b2bb88250dd8c18107e3b79d1ea
Ashish Agarwal <ashisha@hike.in>
2015-09-16 18:07:51 +0530

    adding listener for caller

---
+++ src/com/bsb/hike/chatHead/CallListener.java
@@ -0,0 +1,38 @@
+package com.bsb.hike.chatHead;
+
+import com.bsb.hike.HikeConstants;
+import com.bsb.hike.modules.httpmgr.exception.HttpException;
+import com.bsb.hike.modules.httpmgr.request.listener.IRequestListener;
+import com.bsb.hike.modules.httpmgr.response.Response;
+import com.bsb.hike.utils.HikeSharedPreferenceUtil;
+import com.bsb.hike.utils.Logger;
+
+public class CallListener implements IRequestListener
+{
+	private String number;
+
+	public void setNumber(String number)
+	{
+		this.number = number;
+	}
+	
+	@Override
+	public void onRequestSuccess(Response result)
+	{
+		HikeSharedPreferenceUtil.getInstance(HikeConstants.CALLER_SHARED_PREF).saveData(number, result.getBody().getContent().toString());
+		StickyCaller.showCallerView(number, result.getBody().getContent().toString());
+	}
+
+	@Override
+	public void onRequestProgressUpdate(float progress)
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onRequestFailure(HttpException httpException)
+	{
+		Logger.d("ashish",httpException.toString());
+		// TODO Auto-generated method stub
+	}
+}
