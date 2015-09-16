5726b9c0c5b0fc925d6bfa7fa0e8d679207ff3f2
Ashish Agarwal <ashisha@hike.in>
2015-09-16 21:49:11 +0530

    sticky caller refactoring

--- src/com/bsb/hike/chatHead/StickyCaller.java
+++ src/com/bsb/hike/chatHead/StickyCaller.java
@@ -21,23 +21,22 @@ import com.google.gson.JsonParser;
 
 public class StickyCaller
 {
-	
-private static LinearLayout stickyCallerView;
-	
+
+	private static LinearLayout stickyCallerView;
+
 	static WindowManager windowManager;
-	
+
 	private static short moveType;
-	
+
 	private static final short NONE = 0;
-	
+
 	private static final short HORIZONTAL = 1;
-	
+
 	private static final short VERTICAL = 2;
-	
-	
-	
-	static LayoutParams callerParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, LayoutParams.TYPE_PHONE, LayoutParams.FLAG_NOT_FOCUSABLE | LayoutParams.FLAG_LAYOUT_NO_LIMITS ,
-			PixelFormat.TRANSLUCENT);
+
+	static LayoutParams callerParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, LayoutParams.TYPE_PHONE, LayoutParams.FLAG_NOT_FOCUSABLE
+			| LayoutParams.FLAG_LAYOUT_NO_LIMITS, PixelFormat.TRANSLUCENT);
+
 	private static void actionMove(Context context, int initialX, int initialY, Float initialTouchX, Float initialTouchY, MotionEvent event)
 	{
 		if (ChatHeadLayout.getOverlayView() == null || !ChatHeadLayout.getOverlayView().isShown())
@@ -45,7 +44,7 @@ private static LinearLayout stickyCallerView;
 			if ((moveType == NONE || moveType == HORIZONTAL) && Math.abs(event.getRawX() - initialTouchX) > Math.abs(event.getRawY() - initialTouchY))
 			{
 				callerParams.x = initialX + (int) (event.getRawX() - initialTouchX);
-				callerParams.alpha = (float) (1.0-(Math.abs(callerParams.x)/Utils.getDeviceWidth()));
+				callerParams.alpha =  1.0f - ((float)Math.abs(callerParams.x) / Utils.getDeviceWidth());
 				moveType = HORIZONTAL;
 			}
 			else
@@ -55,7 +54,7 @@ private static LinearLayout stickyCallerView;
 				{
 					callerParams.y = 0;
 				}
-				if (callerParams.y > Utils.getDeviceHeight()- stickyCallerView.getHeight())
+				if (callerParams.y > Utils.getDeviceHeight() - stickyCallerView.getHeight())
 				{
 					callerParams.y = Utils.getDeviceHeight() - stickyCallerView.getHeight();
 				}
@@ -73,8 +72,23 @@ private static LinearLayout stickyCallerView;
 		return;
 	}
 
-	
-	
+	public static void slideAnimation(int from, int to)
+	{
+		ValueAnimator translateX = ValueAnimator.ofInt(from, to);
+		translateX.addUpdateListener(new ValueAnimator.AnimatorUpdateListener()
+		{
+			@Override
+			public void onAnimationUpdate(ValueAnimator valueAnimator)
+			{
+				callerParams.x = (Integer) valueAnimator.getAnimatedValue();
+				callerParams.alpha = 1.0f - ((float)Math.abs(callerParams.x) / Utils.getDeviceWidth());
+				windowManager.updateViewLayout(stickyCallerView, callerParams);
+			}
+		});
+		translateX.setDuration(500);
+		translateX.start();
+	}
+
 	public static void removeCallerView()
 	{
 		Context context = HikeMessengerApp.getInstance();
@@ -84,15 +98,65 @@ private static LinearLayout stickyCallerView;
 			windowManager.removeView(stickyCallerView);
 			stickyCallerView = null;
 		}
-		catch(Exception e)
+		catch (Exception e)
 		{
 			Logger.d("Sticky Caller","Removing Caller View");
 		}
 	}
 
+	static OnSwipeTouchListener onSwipeTouchListener = new OnSwipeTouchListener(HikeMessengerApp.getInstance())
+	{
+		public void onSwipeRight()
+		{
+			stickyCallerView.clearAnimation();
+			slideAnimation(callerParams.x, (Utils.getDeviceWidth()));
 
-	
-	
+		}
+
+		public void onSwipeLeft()
+		{
+			stickyCallerView.clearAnimation();
+			slideAnimation(callerParams.x, -(Utils.getDeviceWidth()));
+
+		}
+
+		int initialX, initialY;
+
+		float initialTouchX, initialTouchY;
+
+		public boolean onTouch(View v, MotionEvent event)
+		{
+			switch (event.getAction())
+			{
+			case MotionEvent.ACTION_DOWN:
+				initialX = callerParams.x;
+				initialY = callerParams.y;
+				initialTouchX = event.getRawX();
+				initialTouchY = event.getRawY();
+				moveType = NONE;
+				break;
+			case MotionEvent.ACTION_UP:
+				if (callerParams.x < -(Utils.getDeviceWidth() / 2))
+				{
+					slideAnimation(callerParams.x, -(Utils.getDeviceWidth()));
+				}
+				else if (callerParams.x > (Utils.getDeviceWidth() / 2))
+				{
+					slideAnimation(callerParams.x, Utils.getDeviceWidth());
+				}
+				else
+				{
+					slideAnimation(callerParams.x, 0);
+				}
+				break;
+			case MotionEvent.ACTION_MOVE:
+				actionMove(HikeMessengerApp.getInstance(), initialX, initialY, initialTouchX, initialTouchY, event);
+				break;
+			}
+			return gestureDetector.onTouchEvent(event);
+		}
+	};
+
 	public static void showCallerView(String number, String result)
 	{
 		final Context context = HikeMessengerApp.getInstance();
@@ -103,83 +167,15 @@ private static LinearLayout stickyCallerView;
 		stickyCallerView = (LinearLayout) inflater.inflate(R.layout.caller_layout, null);
 		stickyCallerView.findViewById(R.id.caller_detail_view).setVisibility(View.VISIBLE);
 		((TextView) (stickyCallerView.findViewById(R.id.caller_number))).setText(number);
-        ((TextView) (stickyCallerView.findViewById(R.id.caller_name))).setText(callerContentModel.getFirsttName()+" "+callerContentModel.getLastName());
+		((TextView) (stickyCallerView.findViewById(R.id.caller_name))).setText(callerContentModel.getFirsttName() + " " + callerContentModel.getLastName());
 		((TextView) (stickyCallerView.findViewById(R.id.caller_location))).setText(callerContentModel.getLocation());
 		windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
 		callerParams.gravity = Gravity.TOP | Gravity.LEFT;
-		callerParams.x =0;
-		callerParams.y=0;
-		callerParams.alpha=(float) 1.0;
+		callerParams.x = 0;
+		callerParams.y = 0;
+		callerParams.alpha = 1.0f;
 		windowManager.addView(stickyCallerView, callerParams);
-		stickyCallerView.setOnTouchListener(new OnSwipeTouchListener(context)
-		{	
-			public void onSwipeRight()
-			{
-				stickyCallerView.clearAnimation();
-				slideAnimation(callerParams.x, (Utils.getDeviceWidth()));
-
-			}
-
-			public void slideAnimation(int from, int to)
-			{
-				ValueAnimator translateX = ValueAnimator.ofInt(from, to);
-				translateX.addUpdateListener(new ValueAnimator.AnimatorUpdateListener()
-				{
-					@Override
-					public void onAnimationUpdate(ValueAnimator valueAnimator)
-					{
-						callerParams.x = (Integer) valueAnimator.getAnimatedValue();
-						callerParams.alpha = (float) (1.0-(Math.abs(callerParams.x)/Utils.getDeviceWidth()));
-						windowManager.updateViewLayout(stickyCallerView, callerParams);
-					}
-				});
-				translateX.setDuration(500);
-				translateX.start();
-			}
-
-			public void onSwipeLeft()
-			{
-				stickyCallerView.clearAnimation();
-				slideAnimation(callerParams.x, -(Utils.getDeviceWidth()));
-
-			}
-
-			int initialX, initialY;
-
-			float initialTouchX, initialTouchY;
-
-			public boolean onTouch(View v, MotionEvent event)
-			{
-				switch (event.getAction())
-				{
-				case MotionEvent.ACTION_DOWN:
-					initialX = callerParams.x;
-					initialY = callerParams.y;
-					initialTouchX = event.getRawX();
-					initialTouchY = event.getRawY();
-					moveType = NONE;
-				     break;
-				case MotionEvent.ACTION_UP:
-					if (callerParams.x < -(Utils.getDeviceWidth() / 2))
-					{
-						slideAnimation(callerParams.x,-(Utils.getDeviceWidth()));
-					}
-					else if (callerParams.x > (Utils.getDeviceWidth() / 2))
-					{
-						slideAnimation(callerParams.x,(Utils.getDeviceWidth()));
-					}
-					else
-					{
-						slideAnimation(callerParams.x, 0);
-					}
-					 break;
-				case MotionEvent.ACTION_MOVE:
-					actionMove(context, initialX, initialY, initialTouchX, initialTouchY, event);
-					break;
-				}
-				return gestureDetector.onTouchEvent(event);
-			}
-		});
+		stickyCallerView.setOnTouchListener(onSwipeTouchListener);
 
 	}
 }
