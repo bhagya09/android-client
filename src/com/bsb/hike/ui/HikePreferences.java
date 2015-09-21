36fbdd4da013fe351409cf16b1a774f4eb5d420a
Anu Bansal <anubansal@Anus-MacBook-Pro.local>
2015-09-21 14:03:59 +0530

    Fix for current language not showing up on settings page, AND-2907

--- src/com/bsb/hike/ui/HikePreferences.java
+++ src/com/bsb/hike/ui/HikePreferences.java
@@ -239,6 +239,7 @@ public class HikePreferences extends HikeAppStateBasePreferenceActivity implemen
 			final ListPreference languagePref = (ListPreference) getPreferenceScreen().findPreference(HikeConstants.KEYBOARD_LANGUAGE_PREF);
 			if (languagePref != null)
 			{
+				KPTAddonItem currentLangItem = null;
 				CharSequence entries[] = new String[mInstalledLanguagesList.size()];
 				CharSequence entryValues[] = new String[mInstalledLanguagesList.size()];
 				int i=0;
@@ -246,10 +247,16 @@ public class HikePreferences extends HikeAppStateBasePreferenceActivity implemen
 				{
 					entries[i] = item.getDisplayName();
 					entryValues[i] = item.getDisplayName();
+					
+					if (item.getDisplayName().equalsIgnoreCase(mCurrentlangName))
+					{
+						currentLangItem = item;
+					}
 					i++;
 				}
 				languagePref.setEntries(entries);
 				languagePref.setEntryValues(entryValues);
+				kptSettings.changeLanguage(currentLangItem);
 				
 				languagePref.setOnPreferenceChangeListener(new OnPreferenceChangeListener()
 				{
