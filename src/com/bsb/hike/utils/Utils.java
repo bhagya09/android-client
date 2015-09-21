34dcdca8e20e89c0eb4fb84ac829c6ff2143063f
Ashish Agarwal <ashisha@hike.in>
2015-09-21 13:44:09 +0530

    adding kill call method

--- src/com/bsb/hike/utils/Utils.java
+++ src/com/bsb/hike/utils/Utils.java
@@ -2858,6 +2858,40 @@ public class Utils
 		}
 		return items;
 	}
+	
+	public static boolean killCall()
+	{
+		Context context = HikeMessengerApp.getInstance();
+		try
+		{
+			// Get the boring old TelephonyManager
+			TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+
+			// Get the getITelephony() method
+			Class classTelephony = Class.forName(telephonyManager.getClass().getName());
+			java.lang.reflect.Method methodGetITelephony = classTelephony.getDeclaredMethod("getITelephony");
+
+			// Ignore that the method is supposed to be private
+			methodGetITelephony.setAccessible(true);
+
+			// Invoke getITelephony() to get the ITelephony interface
+			Object telephonyInterface = methodGetITelephony.invoke(telephonyManager);
+
+			// Get the endCall method from ITelephony
+			Class telephonyInterfaceClass = Class.forName(telephonyInterface.getClass().getName());
+			java.lang.reflect.Method methodEndCall = telephonyInterfaceClass.getDeclaredMethod("endCall");
+
+			// Invoke endCall()
+			methodEndCall.invoke(telephonyInterface);
+
+		}
+		catch (Exception ex)
+		{ // Many things can go wrong with reflection calls
+			return false;
+		}
+		return true;
+	}
+	
 
 	/**
 	 * Get unseen status, user-status and friend request count
