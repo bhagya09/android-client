c3e15097bddb35a321d4d7efae49da200d4a6bfb
ravipradeep89 <ravip@hike.in>
2015-10-01 16:04:11 +0530

    Overloading the getNotificationCount to include the Activity feed if needed

--- src/com/bsb/hike/utils/Utils.java
+++ src/com/bsb/hike/utils/Utils.java
@@ -2831,20 +2831,23 @@ public class Utils
 	}
 
 	/**
-	 * Get unseen status, user-status and friend request count
+	 * Get unseen status, user-status and friend request count,
 	 * 
 	 * @param accountPrefs
 	 *            Account settings shared preference
 	 * @param countUsersStatus
 	 *            Whether to include user status count in the total
+	 * @param countUsersActivity
+	 *            Whether to include user activity count in the total
 	 * @return
 	 */
-	public static int getNotificationCount(SharedPreferences accountPrefs, boolean countUsersStatus)
+	public static int getNotificationCount(SharedPreferences accountPrefs, boolean countUsersStatus, boolean countUserActivity)
 	{
 		int notificationCount = 0;
 
 		notificationCount += accountPrefs.getInt(HikeMessengerApp.UNSEEN_STATUS_COUNT, 0);
-		notificationCount += accountPrefs.getInt(HikeMessengerApp.USER_TIMELINE_ACTIVITY_COUNT, 0);
+		if (countUserActivity)
+			notificationCount += accountPrefs.getInt(HikeMessengerApp.USER_TIMELINE_ACTIVITY_COUNT, 0);
 		if (countUsersStatus)
 		{
 			notificationCount += accountPrefs.getInt(HikeMessengerApp.UNSEEN_USER_STATUS_COUNT, 0);
@@ -2854,19 +2857,19 @@ public class Utils
 		notificationCount += frCount;
 		return notificationCount;
 	}
-	public static int getNotificationCountTimeLineOnly(SharedPreferences accountPrefs, boolean countUsersStatus)
+
+	/**
+	 * Get unseen status, user-status and friend request count,includes activity count as well
+	 * 
+	 * @param accountPrefs
+	 *            Account settings shared preference
+	 * @param countUsersStatus
+	 *            Whether to include user status count in the total
+	 * @return
+	 */
+	public static int getNotificationCount(SharedPreferences accountPrefs, boolean countUsersStatus)
 	{
-		int notificationCount = 0;
-
-		notificationCount += accountPrefs.getInt(HikeMessengerApp.UNSEEN_STATUS_COUNT, 0);
-		if (countUsersStatus)
-		{
-			notificationCount += accountPrefs.getInt(HikeMessengerApp.UNSEEN_USER_STATUS_COUNT, 0);
-		}
-
-		int frCount = accountPrefs.getInt(HikeMessengerApp.FRIEND_REQ_COUNT, 0);
-		notificationCount += frCount;
-		return notificationCount;
+		return getNotificationCount(accountPrefs, countUsersStatus, true);
 	}
 	/*
 	 * This method returns whether the device is an mdpi or ldpi device. The assumption is that these devices are low end and hence a DB call may block the UI on those devices.
