9cdafa0b9927e09888ebf74ccea473aea1a95107
ravipradeep89 <ravip@hike.in>
2015-10-01 16:23:00 +0530

    Removing badger as a library now adding it as a jar

--- badger/src/com/bsb/hike/badger/shortcutbadger/ShortcutBadgeException.java
+++
@@ -1,8 +0,0 @@
-package com.bsb.hike.badger.shortcutbadger;
-
-@Deprecated
-public class ShortcutBadgeException extends Exception {
-    public ShortcutBadgeException(String message) {
-        super(message);
-    }
-}
