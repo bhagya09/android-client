d1512ea2190714f55292e11c3b64d56693ef7f57
Rashmi Dang <you@example.com>
2015-10-03 20:41:09 +0530

    Fixed - AND-3189

--- src/com/bsb/hike/ui/HikePreferences.java
+++ src/com/bsb/hike/ui/HikePreferences.java
@@ -139,8 +139,7 @@ public class HikePreferences extends HikeAppStateBasePreferenceActivity implemen
 			setBlockingTask((ActivityCallableTask) retained);
 			mTask.setActivity(this);
 		}
-
-		if (preferences == R.xml.keyboard_settings_preferences && titleRes == R.string.settings_keyboard)
+       if (preferences == R.xml.keyboard_settings_preferences && titleRes == R.string.settings_keyboard||preferences == R.xml.kpt_advanced_preferences)
 		{
 			kptSettings = new KPTAdaptxtAddonSettings(this, this);
 
