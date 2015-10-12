1f1e98190b5a552cfa27b44a7d12d4afe0ff5c02
Umangjeet Singh Pahwa <umangjeet@hike.in>
2015-10-12 07:59:54 +0530

    fixing AND-1530, retaining stickey head position

--- src/com/bsb/hike/chatHead/ChatHeadViewManager.java
+++ src/com/bsb/hike/chatHead/ChatHeadViewManager.java
@@ -338,6 +338,10 @@ public class ChatHeadViewManager
 
 				switch (flag)
 				{
+				case ChatHeadConstants.REMAINING_ANIMATION:
+					savedPosX = chatHeadParams.x;
+					savedPosY = chatHeadParams.y;
+					break;
 				case ChatHeadConstants.CREATING_CHAT_HEAD_ACTIVITY_ANIMATION:
 //					if (!ChatHeadUtils.getRunningAppPackage(ChatHeadUtils.GET_TOP_MOST_SINGLE_PROCESS).contains(sharableActivePackage))
 //					{
