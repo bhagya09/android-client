75a7abf48a708149982d7919afd99f36a569f5d2
Atul Mittal <atul@hike.in>
2015-10-12 15:41:23 +0530

    removed unused imports

--- src/com/bsb/hike/photos/views/PhotosEditerFrameLayoutView.java
+++ src/com/bsb/hike/photos/views/PhotosEditerFrameLayoutView.java
@@ -12,8 +12,6 @@ import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Matrix;
-import android.graphics.Paint;
 import android.media.MediaScannerConnection;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
