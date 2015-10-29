241781079015658c3b2b36fabe4815951dc10f1a
Anu Bansal <anubansal@Anus-MacBook-Pro-2.local>
2015-10-29 19:11:50 +0530

    Fixed AND-3621

--- src/com/bsb/hike/timeline/view/StatusUpdate.java
+++ src/com/bsb/hike/timeline/view/StatusUpdate.java
@@ -1119,6 +1119,7 @@ public class StatusUpdate extends HikeAppStateBaseFragmentActivity implements Li
 			}
 			else
 			{
+				getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);				
 				mCustomKeyboard.showCustomKeyboard(statusTxt, true);
 				KptUtils.updatePadding(StatusUpdate.this, R.id.parent_layout, (keyboardHeight == 0) ? mCustomKeyboard.getKeyBoardAndCVHeight() : keyboardHeight);
 			}
