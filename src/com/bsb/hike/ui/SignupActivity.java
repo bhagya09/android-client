69f54b8aad2574fea8bad18b0d843eb8b5e1d8a0
gaurav-m <gauravm@bsb.in>
2015-10-29 08:31:40 +0530

    increased tapable area in language selected option in signup

--- src/com/bsb/hike/ui/SignupActivity.java
+++ src/com/bsb/hike/ui/SignupActivity.java
@@ -1263,7 +1263,7 @@ public class SignupActivity extends ChangeProfileImageBaseActivity implements Si
 		final ArrayList<LocalLanguage> list = new ArrayList<>(LocalLanguage.getSupportedLanguages(this));
 		languageText.setText(list.get(0).getDisplayName());
 
-		languageText.setOnClickListener(
+		viewFlipper.findViewById(R.id.lang_select).setOnClickListener(
 		new OnClickListener()
 		{
 			@Override
