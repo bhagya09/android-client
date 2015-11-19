6b3a288b3252c8021989ba6cc03ce5f0e613065f
Atul Mittal <atul@hike.in>
2015-11-19 12:55:29 +0530

    Revert "Revert "refactoring: removed old crop, use new""
    This reverts commit 8115afcc4b774c279b45c24f8f0808416016c2ab.

--- src/com/bsb/hike/cropimage/HikeCropActivity.java
+++ src/com/bsb/hike/cropimage/HikeCropActivity.java
@@ -44,6 +44,7 @@ import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bsb.hike.HikeConstants;
 import com.bsb.hike.R;
 import com.bsb.hike.BitmapModule.BitmapUtils;
 import com.bsb.hike.cropimage.HikeCropFragment.HikeCropListener;
@@ -93,11 +94,30 @@ public class HikeCropActivity extends HikeAppStateBaseFragmentActivity
 		if (extras != null)
 		{
 			mSrcImagePath = extras.getString(SOURCE_IMAGE_PATH);
-			mCropImagePath = extras.getString(CROPPED_IMAGE_PATH);
-			Parcelable parcelable = extras.getParcelable(CROP_COMPRESSION);
-			if (parcelable != null)
+
+			if (TextUtils.isEmpty(mSrcImagePath))
 			{
-				mCropCompression = (CropCompression) parcelable;
+				mSrcImagePath = intent.getStringExtra(HikeConstants.Extras.GALLERY_SELECTION_SINGLE);
+			}
+
+			if (TextUtils.isEmpty(mSrcImagePath))
+			{
+				onCropFailed();
+				return;
+			}
+
+			mCropImagePath = extras.getString(CROPPED_IMAGE_PATH);
+
+			Bundle intentExtraBundle = extras.getBundle(CROP_COMPRESSION);
+
+			if (intentExtraBundle != null)
+			{
+				Parcelable parcelable = intentExtraBundle.getParcelable(CROP_COMPRESSION);
+
+				if (parcelable != null)
+				{
+					mCropCompression = (CropCompression) parcelable;
+				}
 			}
 		}
 
