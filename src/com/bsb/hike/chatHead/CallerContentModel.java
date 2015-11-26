dedb70d6dc53e5d043a9e47b70e79fe5baa7d84e
Ashish Agarwal <ashisha@hike.in>
2015-11-26 13:45:05 +0530

    adding setter and getter caller content model

--- src/com/bsb/hike/chatHead/CallerContentModel.java
+++ src/com/bsb/hike/chatHead/CallerContentModel.java
@@ -30,8 +30,23 @@ public class CallerContentModel
 	@Expose
 	private JsonObject name;
 
+	@Expose
+	private boolean is_spam;
+
+	@Expose
+	private int spam_count;
+
+	private String full_name;
+
+	private boolean is_block;
+
+
 	public String getLastName()
 	{
+		if (full_name != null)
+		{
+			return null;
+		}
 		if (name != null && name.has(LAST_NAME))
 		{
 			return name.get(LAST_NAME).getAsString();
@@ -44,6 +59,11 @@ public class CallerContentModel
 
 	public String getFirstName()
 	{
+		if (full_name != null)
+		{
+			return  full_name;
+		}
+		
 		if (name != null && name.has(FIRST_NAME))
 		{
 			return name.get(FIRST_NAME).getAsString();
@@ -53,6 +73,35 @@ public class CallerContentModel
 			return null;
 		}
 	}
+
+	public String getFullName()
+	{
+		if (full_name != null)
+		{
+			return  full_name;
+		}
+
+		String firstName = getFirstName();
+
+		String lastName = getLastName();
+
+		if (firstName == null && lastName == null)
+		{
+			return null;
+		}
+
+		String name = "";
+		if (firstName != null)
+		{
+			name = firstName + " ";
+		}
+		if (lastName != null)
+		{
+			name = name + lastName;
+		}
+		return name;
+	}
+
 
 	public String getLocation()
 	{
@@ -69,4 +118,53 @@ public class CallerContentModel
 		return msisdn;
 	}
 
+	public int getSpamCount()
+	{
+		return spam_count;
+	}
+
+	public boolean isSpam()
+	{
+		return is_spam;
+	}
+
+	public boolean isBlock()
+	{
+		return is_block;
+	}
+
+	public void setBlock(boolean is_block)
+	{
+		this.is_block = is_block;
+	}
+
+	public void setLocation(String location)
+	{
+		this.location = location;
+	}
+
+	public void setMsisdn(String msisdn)
+	{
+		this.msisdn = msisdn;
+	}
+
+	public void setIsOnHike(boolean is_on_hike)
+	{
+		this.is_on_hike = is_on_hike;
+	}
+
+	public void setSpamCount(int spam_count)
+	{
+		this.spam_count = spam_count;
+	}
+
+	public void setIsSpam(boolean is_spam)
+	{
+		this.is_spam = is_spam;
+	}
+
+	public void setFullName(String full_name)
+	{
+		this.full_name = full_name;
+	}
 }
