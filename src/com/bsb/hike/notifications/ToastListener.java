84022ebe79c30468227924e68d77989a72fbe4dd
Atul Mittal <atul@hike.in>
2015-12-16 13:01:24 +0530

    bug fix AND-4158

--- src/com/bsb/hike/notifications/ToastListener.java
+++ src/com/bsb/hike/notifications/ToastListener.java
@@ -32,6 +32,7 @@ import com.bsb.hike.models.ConvMessage.ParticipantInfoState;
 import com.bsb.hike.models.HikeFile.HikeFileType;
 import com.bsb.hike.modules.contactmgr.ContactManager;
 import com.bsb.hike.platform.HikePlatformConstants;
+import com.bsb.hike.timeline.model.ActionsDataModel;
 import com.bsb.hike.timeline.model.FeedDataModel;
 import com.bsb.hike.timeline.model.StatusMessage;
 import com.bsb.hike.timeline.model.StatusMessage.StatusMessageType;
@@ -185,7 +186,10 @@ public class ToastListener implements Listener
 				FeedDataModel activityFeed = (FeedDataModel) object;
 				if (StealthModeManager.getInstance().isStealthMsisdn(activityFeed.getActor()) && !StealthModeManager.getInstance().isActive())
 				{
-					this.toaster.notifyStealthMessage(NotificationType.HIDDEN,activityFeed.getActor());
+					if (activityFeed.getActionType() == ActionsDataModel.ActionTypes.LIKE)
+					{
+						this.toaster.notifyStealthMessage(NotificationType.HIDDEN, activityFeed.getActor());
+					}
 				}
 				else
 				{
