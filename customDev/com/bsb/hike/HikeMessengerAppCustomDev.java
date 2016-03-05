ddb532d5a162048b4fa19f249cc4a27175df4b4d
piyushghai <piyush@hike.in>
2016-03-05 15:11:49 +0530

    Added LeakCanary to debug builds

--- customDev/com/bsb/hike/HikeMessengerAppCustomDev.java
+++ customDev/com/bsb/hike/HikeMessengerAppCustomDev.java
@@ -2,6 +2,7 @@ package com.bsb.hike;
 
 import com.bsb.hike.utils.Logger;
 import com.facebook.stetho.Stetho;
+import com.squareup.leakcanary.LeakCanary;
 
 /**
  * Created by piyush on 25/02/16.
@@ -14,8 +15,9 @@ public class HikeMessengerAppCustomDev extends HikeMessengerApp
 	public void onCreate()
 	{
 		super.onCreate();
-		Logger.d(TAG, "Running HikeMessengerApp : debug" + "Adding Stetho now");
+		Logger.d(TAG, "Running HikeMessengerAppCustomDevDebug : " + "Injecting Stetho + LeakCanary Libraries");
 		Stetho.initialize(Stetho.newInitializerBuilder(this).enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
 				.enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this)).build());
+		LeakCanary.install(this);
 	}
 }
