10effc17574efbf09c79e46e0d82633343325529
piyushghai <piyush@hike.in>
2016-03-16 15:04:36 +0530

    Added DB Restore AsyncTask

---
+++ src/com/bsb/hike/db/DBRestoreAsyncTask.java
@@ -0,0 +1,62 @@
+package com.bsb.hike.db;
+
+import android.os.AsyncTask;
+
+import com.bsb.hike.HikeMessengerApp;
+import com.bsb.hike.backup.AccountBackupRestore;
+
+import static com.bsb.hike.backup.AccountBackupRestore.RestoreErrorStates;
+
+/**
+ * This Async Task is used to performing restore operation when db goes kaput!
+ * Created by piyush on 16/03/16.
+ */
+public class DBRestoreAsyncTask extends AsyncTask<Void, Void, Integer>
+{
+	private IRestoreCallback mCallback;
+
+	public DBRestoreAsyncTask(IRestoreCallback mCallback)
+	{
+		this.mCallback = mCallback;
+	}
+
+	@Override
+	protected void onPreExecute()
+	{
+		if (mCallback != null)
+		{
+			mCallback.preRestoreSetup();
+		}
+	}
+
+	@Override
+	protected Integer doInBackground(Void... params)
+	{
+		return AccountBackupRestore
+				.getInstance(HikeMessengerApp.getInstance().getApplicationContext()).restore();
+	}
+
+	@Override
+	protected void onPostExecute(@RestoreErrorStates Integer result)
+	{
+		if (mCallback != null)
+		{
+			mCallback.postRestoreFinished(result);
+		}
+	}
+
+	public void setRestoreCallback(IRestoreCallback mCallback)
+	{
+		this.mCallback = mCallback;
+	}
+
+	/**
+	 * Use this interface if you require to communicate between the caller of this async task and the task itself
+	 */
+	public interface IRestoreCallback
+	{
+		public void preRestoreSetup();
+
+		public void postRestoreFinished(@RestoreErrorStates Integer restoreResult);
+	}
+}
