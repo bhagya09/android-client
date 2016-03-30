f60961d31cdf2b91e70b563e7d4382d256dc1a30
piyushghai <piyush@hike.in>
2016-03-30 13:17:35 +0530

    Added flags in ac packet to determine whether it's a returning user or not

--- src/com/bsb/hike/HikeConstants.java
+++ src/com/bsb/hike/HikeConstants.java
@@ -4056,4 +4056,6 @@ public class HikeConstants
 	public static final String LOG_SQLITE_PROPERTIES = "log_sqlite_props";
 
 	public static final String FAV_TO_FRIENDS_MIGRATION = "f2f_mig";
+
+	public static final String IS_NEW_USER = "is_new_user";
 }
