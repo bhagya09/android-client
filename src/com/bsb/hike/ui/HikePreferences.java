22e1130a1703666b526b95a88e48a57e269a791a
gaurav-m <gauravm@bsb.in>
2016-03-30 16:09:06 +0530

    removed all and my contacts options from the last seen privacy setting

--- src/com/bsb/hike/ui/HikePreferences.java
+++ src/com/bsb/hike/ui/HikePreferences.java
@@ -1724,7 +1724,7 @@ public class HikePreferences extends HikeAppStateBasePreferenceActivity implemen
 
 	private String getLSSummaryText()
 	{
-		String defValue = getApplicationContext().getString(R.string.privacy_my_contacts);
+		String defValue = getApplicationContext().getString(R.string.privacy_favorites);
 		String selectedValue = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString(HikeConstants.LAST_SEEN_PREF_LIST, defValue);
 		String summaryTxt = null;
 		int selIndex = Integer.parseInt(selectedValue);
