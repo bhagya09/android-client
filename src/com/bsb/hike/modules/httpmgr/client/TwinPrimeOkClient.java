00fb874212acaf49f2a2f656bed7c06658dfddf5
Himanshu Khandelwal <himanshu@hike.in>
2016-03-31 14:30:45 +0530

    Now using tp cleint

--- src/com/bsb/hike/modules/httpmgr/client/TwinPrimeOkClient.java
+++ src/com/bsb/hike/modules/httpmgr/client/TwinPrimeOkClient.java
@@ -1,6 +1,7 @@
 package com.bsb.hike.modules.httpmgr.client;
 
 import com.squareup.okhttp.OkHttpClient;
+import com.twinprime.TwinPrimeSDK.TPOkHttpClient;
 
 /**
  * Created by himanshu on 28/03/16.
@@ -10,7 +11,7 @@ public class TwinPrimeOkClient extends OkClient {
     protected OkHttpClient generateClient(ClientOptions clientOptions)
     {
         clientOptions = clientOptions != null ? clientOptions : ClientOptions.getDefaultClientOptions();
-        OkHttpClient client = new OkHttpClient();
+        OkHttpClient client = new TPOkHttpClient();
         addLogging(client);
         return setClientParameters(client, clientOptions);
     }
