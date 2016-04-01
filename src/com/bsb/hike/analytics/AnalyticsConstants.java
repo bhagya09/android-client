49469a7e7e82c035810f51ca0b94b328627cb50f
Varun Arora <varunarora@Varunas-MacBook-Pro.local>
2016-04-01 16:31:05 +0530

    AND-5180

--- src/com/bsb/hike/analytics/AnalyticsConstants.java
+++ src/com/bsb/hike/analytics/AnalyticsConstants.java
@@ -728,4 +728,10 @@ public class AnalyticsConstants
 	public static final String JUST_OPENED = "justOpened";
 	public static final String CHROME_TABS_SUPPORTED = "chromeTabsSupported";
 	public static final String CHROME_TABS_UNSUPPORTED = "chromeTabsUnSupported";
+
+	public static final String PLATFORM_RICH_NOTIFS = "platformRichNotifs";
+	public static final String BITMAP_DOWNLOAD_SUCCESS = "bitmapDownloadSuccess";
+	public static final String BITMAP_DOWNLOAD_UNSUCESSFULL = "bitmapDownloadUnsuccessful";
+	public static final String AUTO_DOWNLOAD_OFF = "autoDownloadOff";
+	public static final String REQUEST_FAILURE = "requestFailure";
 }
