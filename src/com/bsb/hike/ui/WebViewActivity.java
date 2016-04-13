e88b68f773cdeaf3bcfdbdc8c4096528414a7c54
Rashmi Dang <you@example.com>
2016-04-13 14:25:48 +0530

    Fixed nullpointer exception

--- src/com/bsb/hike/ui/WebViewActivity.java
+++ src/com/bsb/hike/ui/WebViewActivity.java
@@ -846,7 +846,7 @@ public class WebViewActivity extends HikeAppStateBaseFragmentActivity implements
 			}
 		}
 
-		if(isBackToActivity.equalsIgnoreCase("true")){
+		if(!TextUtils.isEmpty(isBackToActivity) &&  isBackToActivity.equalsIgnoreCase("true")){
 			this.finish();
 			return;
 		}
