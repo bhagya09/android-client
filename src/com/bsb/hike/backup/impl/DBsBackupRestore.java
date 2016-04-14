abf5fdd60b6df9551f35b6596d13f25d8a846acc
Atul Mittal <atul@hike.in>
2016-04-14 19:15:27 +0530

    added sticker table and category table to backup

--- src/com/bsb/hike/backup/impl/DBsBackupRestore.java
+++ src/com/bsb/hike/backup/impl/DBsBackupRestore.java
@@ -36,7 +36,7 @@ public class DBsBackupRestore implements BackupableRestorable
 	{
 		chatsDB = new DB(DBConstants.CONVERSATIONS_DATABASE_NAME,
 				// STICKER_SHOP_TABLE and STICKER_CATEGORIES_TABLE will be skipped
-				new String[] { DBConstants.STICKER_SHOP_TABLE, DBConstants.STICKER_CATEGORIES_TABLE,DBConstants.STICKER_TABLE },
+				new String[] { DBConstants.STICKER_SHOP_TABLE},
 				backupToken)
 		{
 			@Override
