dabf3f494d368a68235cabdd9afde43cd4a3144a
piyushghai <piyush@hike.in>
2016-04-18 12:25:42 +0530

    Revert "Using the new file path for CustomStickerCategories as well now"
    This reverts commit 349d5fcc4d911d9714e0867568463c8343966b17.

--- src/com/bsb/hike/models/CustomStickerCategory.java
+++ src/com/bsb/hike/models/CustomStickerCategory.java
@@ -97,7 +97,7 @@ public class CustomStickerCategory extends StickerCategory
 
 	public void loadStickers()
 	{
-		stickerSet = getSortedListForCategory(getCategoryId(), StickerManager.getInstance().getStickerCategoryDirPath(getCategoryId()));
+		stickerSet = getSortedListForCategory(getCategoryId(), StickerManager.getInstance().getInternalStickerDirectoryForCategoryId(getCategoryId()));
 		if (getCategoryId().equals(StickerManager.RECENT))
 		{
 			if(stickerSet.isEmpty())
