4e4db65e784da81bc07444ca322906f1366a024d
Param Shah <param@hike.in>
2016-04-23 22:35:51 +0530

    added logic for atomic tip creation

--- src/com/bsb/hike/productpopup/AtomicTipManager.java
+++ src/com/bsb/hike/productpopup/AtomicTipManager.java
@@ -1,10 +1,12 @@
 package com.bsb.hike.productpopup;
 
 import android.os.Handler;
+import android.view.View;
 
 import com.bsb.hike.db.HikeContentDatabase;
 import com.bsb.hike.HikeMessengerApp;
 import com.bsb.hike.models.HikeHandlerUtil;
+import com.bsb.hike.utils.Logger;
 
 import org.json.JSONObject;
 
@@ -22,10 +24,15 @@ public class AtomicTipManager
 
     private ArrayList<AtomicTipContentModel> tipContentModels;
 
+    private static final String TAG = "AtomicTipManager";
+
+    private static AtomicTipContentModel currentlyShowing;
+
     private AtomicTipManager()
     {
         mHandler = new Handler(HikeHandlerUtil.getInstance().getLooper());
         tipContentModels = new ArrayList<>();
+        currentlyShowing = null;
     }
 
     public static AtomicTipManager getInstance()
@@ -87,5 +94,56 @@ public class AtomicTipManager
                 HikeContentDatabase.getInstance().flushAtomicTipTable();
             }
         });
+    }
+
+    /**
+     * Method to check if any atomic tip exists by checking size of tips array list
+     * @return
+     */
+    public boolean doesAtomicTipExist()
+    {
+        return (tipContentModels.size() > 0);
+    }
+
+    /**
+     * Method to check if any unseen tip exists. Since tips were retrieved from DB in order of status
+     * and priority, all unseen tips are at top of list
+     * @return
+     */
+    public boolean doesUnseenTipExist()
+    {
+        return (doesAtomicTipExist() && tipContentModels.get(0).getTipStatus() == AtomicTipContentModel.AtomicTipStatus.UNSEEN.getValue());
+    }
+
+    /**
+     * Method to check if any high priority (<2) tip exists. Since tips were retrieved from DB in order of
+     * status and priority, all high priority tips (for any given status) will appear first
+     * @return
+     */
+    public boolean doesHighPriorityTipExist()
+    {
+        return (doesAtomicTipExist() && tipContentModels.get(0).getPriority() < 2);
+    }
+
+    public AtomicTipContentModel getFirstAtomicTip()
+    {
+        return tipContentModels.get(0);
+    }
+
+    public View getAtomicTipView(AtomicTipContentModel tipContentModel)
+    {
+        currentlyShowing = tipContentModel;
+        Logger.d(TAG, "inflating atomic tip view");
+        return  null;
+    }
+
+    public void onAtomicTipClicked()
+    {
+        Logger.d(TAG, "processing atomic tip click action");
+    }
+
+    public void onAtomicTipClosed()
+    {
+        Logger.d(TAG, "processing atomic tip dismiss");
     }
 }
