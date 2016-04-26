413085bb2a4a46ccfb9c028471fbff3958fa7d02
Akhil Tripathi <akhil@hike.in>
2016-04-26 10:17:20 +0530

    Formatting

--- src/com/bsb/hike/modules/stickersearch/ui/CategorySearchWatcher.java
+++ src/com/bsb/hike/modules/stickersearch/ui/CategorySearchWatcher.java
@@ -10,7 +10,7 @@ import com.bsb.hike.modules.stickersearch.provider.db.CategorySearchManager;
 import com.bsb.hike.ui.fragments.StickerShopSearchFragment;
 import com.bsb.hike.utils.Logger;
 
-public class CategorySearchWatcher implements CategorySearchListener,SearchView.OnQueryTextListener
+public class CategorySearchWatcher implements CategorySearchListener, SearchView.OnQueryTextListener
 {
 	public static final String TAG = CategorySearchWatcher.class.getSimpleName();
 
@@ -19,7 +19,7 @@ public class CategorySearchWatcher implements CategorySearchListener,SearchView.
 	public CategorySearchWatcher(CategorySearchListener listener)
 	{
 		Logger.i(TAG, "Initialising Category tag watcher...");
-        this.mListener = listener;
+		this.mListener = listener;
 	}
 
 	public void releaseResources()
@@ -31,43 +31,47 @@ public class CategorySearchWatcher implements CategorySearchListener,SearchView.
 	@Override
 	public void onSearchCompleted(final List<StickerCategory> categories)
 	{
-        CategorySearchManager.getInstance().getSearchEngine().runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                if(mListener == null)
-                {
-                    Logger.e(TAG,"onSearchCompleted() : No listener present");
-                }
-                mListener.onSearchCompleted(categories);
-            }
-        },0);
+		CategorySearchManager.getInstance().getSearchEngine().runOnUiThread(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				if (mListener == null)
+				{
+					Logger.e(TAG, "onSearchCompleted() : No listener present");
+				}
+				mListener.onSearchCompleted(categories);
+			}
+		}, 0);
 	}
 
 	@Override
 	public void onNoCategoriesFound(final String query)
 	{
-        CategorySearchManager.getInstance().getSearchEngine().runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                if(mListener == null)
-                {
-                    Logger.e(TAG,"onNoCategoriesFound() : No listener present");
-                }
+		CategorySearchManager.getInstance().getSearchEngine().runOnUiThread(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				if (mListener == null)
+				{
+					Logger.e(TAG, "onNoCategoriesFound() : No listener present");
+				}
 
-                mListener.onNoCategoriesFound(query);
-            }
-        },0);
+				mListener.onNoCategoriesFound(query);
+			}
+		}, 0);
 	}
 
 	@Override
 	public boolean onQueryTextSubmit(String query)
 	{
-         return CategorySearchManager.getInstance().onQueryTextSubmit(query,this);
+		return CategorySearchManager.getInstance().onQueryTextSubmit(query, this);
 	}
 
 	@Override
 	public boolean onQueryTextChange(String query)
 	{
-        return CategorySearchManager.getInstance().onQueryTextChange(query, this);
+		return CategorySearchManager.getInstance().onQueryTextChange(query, this);
 	}
 }
