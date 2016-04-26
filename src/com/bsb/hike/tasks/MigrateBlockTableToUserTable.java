24d5178d741be17ff9431843301d5c471ab688f5
Himanshu Khandelwal <himanshu@hike.in>
2016-04-26 18:09:39 +0530

    Adding a migration class to migrate block to user table

---
+++ src/com/bsb/hike/tasks/MigrateBlockTableToUserTable.java
@@ -0,0 +1,36 @@
+package com.bsb.hike.tasks;
+
+import com.bsb.hike.modules.contactmgr.ContactManager;
+import com.bsb.hike.modules.contactmgr.HikeUserDatabase;
+import com.bsb.hike.utils.Utils;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+/**
+ * Created by himanshu on 26/04/16.
+ */
+
+/**
+ * This class will be used to migrate block and favorite table.
+ */
+public class MigrateBlockTableToUserTable implements Callable<Boolean> {
+
+    @Override
+    public Boolean call() throws Exception {
+
+        List<String> blockedMsisdn = HikeUserDatabase.getInstance().getBlockedMsisdnFromBlockTable();
+        if (Utils.isEmpty(blockedMsisdn)) {
+            return true;
+        }
+
+        // Move to User Table
+        ContactManager.getInstance().block(blockedMsisdn);
+
+        //Drop Block Table
+        HikeUserDatabase.getInstance().dropBlockTable();
+
+        return true;
+
+    }
+}
