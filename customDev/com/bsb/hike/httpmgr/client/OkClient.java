d412bb71b7234763a894a383445e7d02ba72e670
Sidharth <sidharth@hike.in>
2016-04-26 14:12:23 +0530

    moved static methods to separte factory class

--- customDev/com/bsb/hike/httpmgr/client/OkClient.java
+++ customDev/com/bsb/hike/httpmgr/client/OkClient.java
@@ -44,108 +44,9 @@ public class OkClient implements com.bsb.hike.modules.httpmgr.client.IClient
 		System.setProperty("http.maxConnections", Integer.toString(numOfMaxConnections));
 	}
 
-	/**
-	 * Generates a new OkHttpClient with given clientOption parameters
-	 *
-	 * @param clientOptions
-	 * @return
-	 */
-	static OkHttpClient generateClient(
-			com.bsb.hike.modules.httpmgr.client.ClientOptions clientOptions)
+	protected OkHttpClient generateClient(ClientOptions options)
 	{
-		clientOptions = clientOptions != null ? clientOptions : com.bsb.hike.modules.httpmgr.client.ClientOptions
-				.getDefaultClientOptions();
-		OkHttpClient client = new OkHttpClient();
-		client.networkInterceptors().add(new StethoInterceptor());
-		return setClientParameters(client, clientOptions);
-	}
-
-	/**
-	 * Sets Client option parameters to given OkHttpClient
-	 *
-	 * @param client
-	 * @param clientOptions
-	 * @return
-	 */
-	static OkHttpClient setClientParameters(OkHttpClient client, com.bsb.hike.modules.httpmgr.client.ClientOptions clientOptions)
-	{
-		client.setConnectTimeout(clientOptions.getConnectTimeout(), TimeUnit.MILLISECONDS);
-		client.setReadTimeout(clientOptions.getReadTimeout(), TimeUnit.MILLISECONDS);
-		client.setWriteTimeout(clientOptions.getWriteTimeout(), TimeUnit.MILLISECONDS);
-		client.setWriteTimeout(clientOptions.getWriteTimeout(), TimeUnit.MILLISECONDS);
-		client.setSocketFactory(client.getSocketFactory());
-		client.setSslSocketFactory(clientOptions.getSslSocketFactory());
-		client.setHostnameVerifier(clientOptions.getHostnameVerifier());
-
-		if (clientOptions.getProxy() != null)
-		{
-			client.setProxy(clientOptions.getProxy());
-		}
-
-		if (clientOptions.getProxySelector() != null)
-		{
-			client.setProxySelector(clientOptions.getProxySelector());
-		}
-
-		if (clientOptions.getProxySelector() != null)
-		{
-			client.setProxySelector(clientOptions.getProxySelector());
-		}
-
-		if (clientOptions.getCookieHandler() != null)
-		{
-			client.setCookieHandler(clientOptions.getCookieHandler());
-		}
-
-		if (clientOptions.getCache() != null)
-		{
-			client.setCache(clientOptions.getCache());
-		}
-
-		if (clientOptions.getCache() != null)
-		{
-			client.setCache(clientOptions.getCache());
-		}
-
-		if (clientOptions.getHostnameVerifier() != null)
-		{
-			client.setHostnameVerifier(clientOptions.getHostnameVerifier());
-		}
-
-		if (clientOptions.getCertificatePinner() != null)
-		{
-			client.setCertificatePinner(clientOptions.getCertificatePinner());
-		}
-
-		if (clientOptions.getAuthenticator() != null)
-		{
-			client.setAuthenticator(clientOptions.getAuthenticator());
-		}
-
-		if (clientOptions.getProtocols() != null)
-		{
-			client.setProtocols(clientOptions.getProtocols());
-		}
-
-		if (clientOptions.getConnectionSpecs() != null)
-		{
-			client.setConnectionSpecs(clientOptions.getConnectionSpecs());
-		}
-
-		if (clientOptions.getDispatcher() != null)
-		{
-			client.setDispatcher(clientOptions.getDispatcher());
-		}
-
-		if (clientOptions.getConnectionPool() != null)
-		{
-			client.setConnectionPool(clientOptions.getConnectionPool());
-		}
-
-		client.setFollowSslRedirects(clientOptions.getFollowSslRedirects());
-		client.setFollowRedirects(clientOptions.getFollowRedirects());
-
-		return client;
+		return new OkHttpClientFactory().generateClient(options);
 	}
 
 	public OkClient()
@@ -236,7 +137,7 @@ public class OkClient implements com.bsb.hike.modules.httpmgr.client.IClient
 	 */
 	public OkClient clone(com.bsb.hike.modules.httpmgr.client.ClientOptions clientOptions)
 	{
-		return new OkClient(setClientParameters(client.clone(), clientOptions));
+		return new OkClient(new OkHttpClientFactory().setClientParameters(client.clone(), clientOptions));
 	}
 
 }
