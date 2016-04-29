fb4a9328022c6fec7a978326b11b3d8317037062
Akhil Tripathi <akhil@hike.in>
2016-04-29 19:58:40 +0530

    Formatting

--- src/com/bsb/hike/modules/stickersearch/tasks/CategorySearchTask.java
+++ src/com/bsb/hike/modules/stickersearch/tasks/CategorySearchTask.java
@@ -5,45 +5,48 @@ import java.util.List;
 import com.bsb.hike.models.StickerCategory;
 import com.bsb.hike.modules.stickersearch.listeners.CategorySearchListener;
 import com.bsb.hike.modules.stickersearch.provider.db.CategorySearchManager;
+import com.bsb.hike.utils.Utils;
 
-public class CategorySearchTask implements Runnable {
-    private String query;
+public class CategorySearchTask implements Runnable
+{
+	private String query;
 
-    private CategorySearchListener mListener;
+	private CategorySearchListener mListener;
 
-    private boolean performPartialSearch;
+	private boolean performPartialSearch;
 
-    public CategorySearchTask(String query, CategorySearchListener listener, boolean partialSearch) {
-        this.query = preProcessQuery(query);
-        this.mListener = listener;
-        this.performPartialSearch = partialSearch;
-    }
+	public CategorySearchTask(String query, CategorySearchListener listener, boolean partialSearch)
+	{
+		this.query = preProcessQuery(query);
+		this.mListener = listener;
+		this.performPartialSearch = partialSearch;
+	}
 
-    @Override
-    public void run()
-    {
-        List<StickerCategory> results = CategorySearchManager.getInstance().searchForPacks(query);
+	@Override
+	public void run()
+	{
+		List<StickerCategory> results = CategorySearchManager.getInstance().searchForPacks(query);
 
-        if(!performPartialSearch)
-        {
-            sendResponse(results);
-            return;
-        }
+		if (!performPartialSearch)
+		{
+			sendResponse(results);
+			return;
+		}
 
-        if(results == null && query.indexOf(' ')>0)
-        {
-            sendResponse(CategorySearchManager.getInstance().searchForPacks(query.substring(0, query.lastIndexOf(' '))));
-        }
-        else
-        {
-            sendResponse(results);
-        }
+		if (Utils.isEmpty(results) && query.indexOf(' ') > 0)
+		{
+			sendResponse(CategorySearchManager.getInstance().searchForPacks(query.substring(0, query.lastIndexOf(' '))));
+		}
+		else
+		{
+			sendResponse(results);
+		}
 
-    }
+	}
 
-    private void sendResponse(List<StickerCategory> results)
-    {
-		if (results == null)
+	private void sendResponse(List<StickerCategory> results)
+	{
+		if (Utils.isEmpty(results))
 		{
 			mListener.onNoCategoriesFound(query);
 		}
@@ -51,10 +54,10 @@ public class CategorySearchTask implements Runnable {
 		{
 			mListener.onSearchCompleted(results);
 		}
-    }
+	}
 
-    private String preProcessQuery(String query)
-    {
-        return query.trim().toLowerCase();
-    }
+	private String preProcessQuery(String query)
+	{
+		return query.trim().toLowerCase();
+	}
 }
