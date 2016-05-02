8fb8ab23b2090802a2edc29c4a358e7c908c12d4
piyushghai <piyush@hike.in>
2016-05-02 16:51:10 +0530

    Now showing an informative dialog, post backup restore incase user has changed devices. the dialog contains info for restoring stickers

--- src/com/bsb/hike/tasks/SignupTask.java
+++ src/com/bsb/hike/tasks/SignupTask.java
@@ -51,6 +51,7 @@ import com.bsb.hike.utils.StealthModeManager;
 import com.bsb.hike.utils.StickerManager;
 import com.bsb.hike.utils.Utils;
 import com.crashlytics.android.Crashlytics;
+import com.bsb.hike.backup.BackupUtils;
 
 public class SignupTask extends AsyncTask<Void, SignupTask.StateValue, Boolean> implements ActivityCallableTask
 {
@@ -120,8 +121,8 @@ public class SignupTask extends AsyncTask<Void, SignupTask.StateValue, Boolean> 
 
 	public enum State
 	{
-		MSISDN, ADDRESSBOOK, NAME, PULLING_PIN, PIN, ERROR, PROFILE_IMAGE, GENDER, SCANNING_CONTACTS, PIN_VERIFIED, BACKUP_AVAILABLE, RESTORING_BACKUP, RESTORING_CLOUD_SETTINGS
-	};
+		MSISDN, ADDRESSBOOK, NAME, PULLING_PIN, PIN, ERROR, PROFILE_IMAGE, GENDER, SCANNING_CONTACTS, PIN_VERIFIED, BACKUP_AVAILABLE, RESTORING_BACKUP, RESTORING_CLOUD_SETTINGS, SHOW_STICKER_RESTORE_DIALOG
+	}
 
 	public class StateValue
 	{
@@ -706,7 +707,21 @@ public class SignupTask extends AsyncTask<Void, SignupTask.StateValue, Boolean> 
 					{
 						BotUtils.postAccountRestoreSetup();
 						publishProgress(new StateValue(State.RESTORING_BACKUP,Boolean.TRUE.toString()));
+
+						if (BackupUtils.isDeviceDpiDifferent())
+						{
+							synchronized (this)
+							{
+								this.wait(600); //This is the duration of the success animation. Need to wait for that much amount so as to not interrupt the animation
+							}
+							publishProgress(new StateValue(State.SHOW_STICKER_RESTORE_DIALOG, null)); //Give a signal to show the sticker restore dialog.
+							synchronized (this)
+							{
+								this.wait(); // We will show a sticker dialog here since, the user has restored stickers on a diff device
+							}
+						}
 					}
+
 					else
 					{
 						BotUtils.initBots();
