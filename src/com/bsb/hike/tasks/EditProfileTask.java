b6e5179eeb6ae2d0791628d2b846bfa209a36bec
Sidharth <sidharth@hike.in>
2016-05-03 12:12:59 +0530

    added edit profile name method in EditProfilTask

--- src/com/bsb/hike/tasks/EditProfileTask.java
+++ src/com/bsb/hike/tasks/EditProfileTask.java
@@ -1,7 +1,20 @@
 package com.bsb.hike.tasks;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.bsb.hike.HikeMessengerApp;
+import com.bsb.hike.modules.httpmgr.RequestToken;
+import com.bsb.hike.modules.httpmgr.exception.HttpException;
+import com.bsb.hike.modules.httpmgr.hikehttp.HttpRequests;
 import com.bsb.hike.modules.httpmgr.hikehttp.IHikeHTTPTask;
+import com.bsb.hike.modules.httpmgr.request.listener.IRequestListener;
+import com.bsb.hike.modules.httpmgr.response.Response;
 import com.bsb.hike.ui.ProfileActivity;
+import com.bsb.hike.utils.Logger;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by sidharth on 03/05/16.
@@ -20,6 +33,10 @@ public class EditProfileTask implements IHikeHTTPTask
 
     private boolean isBackPressed;
 
+    private SharedPreferences prefs;
+
+    private RequestToken editNameRequestToken;
+
     public EditProfileTask(String msisdn, ProfileActivity.ProfileType profileType, String newName, String newEmail, int newGenderType, boolean isBackPressed)
     {
         this.msisdn = msisdn;
@@ -28,6 +45,7 @@ public class EditProfileTask implements IHikeHTTPTask
         this.newEmail = newEmail;
         this.newGenderType = newGenderType;
         this.isBackPressed = isBackPressed;
+        this.prefs = HikeMessengerApp.getInstance().getSharedPreferences(HikeMessengerApp.ACCOUNT_SETTINGS, Context.MODE_PRIVATE);
     }
 
     public void setNewName(String name)
@@ -49,6 +67,52 @@ public class EditProfileTask implements IHikeHTTPTask
     public void execute()
     {
 
+    }
+
+    private void editProfileName()
+	{
+		JSONObject json = new JSONObject();
+		try
+		{
+			json.put("name", newName);
+
+			if (this.profileType == ProfileActivity.ProfileType.GROUP_INFO)
+			{
+				editNameRequestToken = HttpRequests.editGroupProfileNameRequest(json, getEditNameRequestListener(), msisdn);
+			}
+			else
+			{
+				editNameRequestToken = HttpRequests.editProfileNameRequest(json, getEditNameRequestListener());
+			}
+			editNameRequestToken.execute();
+		}
+		catch (JSONException e)
+		{
+			Logger.e("ProfileActivity", "Could not set name", e);
+		}
+	}
+
+    private IRequestListener getEditNameRequestListener()
+    {
+        return new IRequestListener()
+        {
+            @Override
+            public void onRequestSuccess(Response result)
+            {
+
+            }
+
+            @Override
+            public void onRequestProgressUpdate(float progress)
+            {
+            }
+
+            @Override
+            public void onRequestFailure(HttpException httpException)
+            {
+
+            }
+        };
     }
 
     @Override
