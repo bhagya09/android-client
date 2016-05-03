d59a24f35f592ea076300c7a0be453ae049ce91b
Sidharth <sidharth@hike.in>
2016-05-03 10:47:10 +0530

    added edit profile name constant in HttpRequestConstants

--- src/com/bsb/hike/modules/httpmgr/hikehttp/HttpRequestConstants.java
+++ src/com/bsb/hike/modules/httpmgr/hikehttp/HttpRequestConstants.java
@@ -591,4 +591,9 @@ public class HttpRequestConstants
 	{
 		return  BASE_URL + BASE_V5 + HIKE_SETTINGS;
 	}
+
+	public static String editProfileNameBaseUrl()
+	{
+		return BASE_URL + BASE_V1 + BASE_ACCOUNT + "/name";
+	}
 }
