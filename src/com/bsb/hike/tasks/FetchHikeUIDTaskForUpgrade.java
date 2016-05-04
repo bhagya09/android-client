2fa9b0f46c2f757962d59b0e2cae66ea800650bc
Himanshu Khandelwal <himanshu@hike.in>
2016-05-04 23:28:38 +0530

    Adding a Task to fetch the UID from server on upgrade

---
+++ src/com/bsb/hike/tasks/FetchHikeUIDTaskForUpgrade.java
@@ -0,0 +1,172 @@
+package com.bsb.hike.tasks;
+
+import com.bsb.hike.HikeMessengerApp;
+import com.bsb.hike.models.ContactInfo;
+import com.bsb.hike.models.FetchUIDTaskPojo;
+import com.bsb.hike.modules.contactmgr.ContactManager;
+import com.bsb.hike.modules.contactmgr.HikeUserDatabase;
+import com.bsb.hike.modules.httpmgr.RequestToken;
+import com.bsb.hike.modules.httpmgr.exception.HttpException;
+import com.bsb.hike.modules.httpmgr.hikehttp.HttpRequests;
+import com.bsb.hike.modules.httpmgr.hikehttp.IHikeHTTPTask;
+import com.bsb.hike.modules.httpmgr.hikehttp.IHikeHttpTaskResult;
+import com.bsb.hike.modules.httpmgr.request.listener.IRequestListener;
+import com.bsb.hike.modules.httpmgr.response.Response;
+import com.bsb.hike.utils.Utils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by himanshu on 04/05/16.
+ */
+public class FetchHikeUIDTaskForUpgrade implements IHikeHTTPTask, IHikeHttpTaskResult, IRequestListener {
+
+
+    private Set<String> addressBookContact;
+    private Set<String> activeChats;
+    private Set<String> bots;
+    private RequestToken token;
+
+    public FetchHikeUIDTaskForUpgrade() {
+
+    }
+
+    private JSONObject getPostData() {
+        JSONObject data = new JSONObject();
+        try {
+            JSONArray addressBook = new JSONArray();
+            addressBookContact.removeAll(activeChats);
+
+            data.put("addr", getJSONArrayFromSet(addressBook, addressBookContact));
+
+            JSONArray activeChat = new JSONArray();
+            data.put("subs", getJSONArrayFromSet(activeChat, activeChats));
+
+            JSONArray bot = new JSONArray();
+            data.put("bots", getJSONArrayFromSet(bot, bots));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    private JSONArray getJSONArrayFromSet(JSONArray arr, Set<String> data) {
+        for (String s : data) {
+            arr.put(s);
+        }
+        return arr;
+    }
+
+    private Set<FetchUIDTaskPojo> parseJSONArrayIntoSet(Set<FetchUIDTaskPojo> data, JSONArray array) {
+
+        if (Utils.isEmpty(array)) {
+            return data;
+        }
+        if (Utils.isEmpty(data)) {
+            data = new HashSet<>(array.length());
+        }
+
+        for (int i = 0; i < array.length(); i++) {
+            try {
+                data.add(new FetchUIDTaskPojo(array.getJSONObject(i).getString("m"), array.getJSONObject(i).getString("u")));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return data;
+    }
+
+    @Override
+    public void execute() {
+        getMsisdnForMissingUID();
+        JSONObject d = getPostData();
+        token = HttpRequests.fetchUIDForMissingMsisdn(getRequestId(), this,d);
+
+        if (!token.isRequestRunning()) {
+            token.execute();
+        }
+
+
+    }
+
+    public String getRequestId() {
+        return "reqId";
+    }
+
+    @Override
+    public void cancel() {
+
+        if (token != null)
+            token.cancel();
+
+    }
+
+    @Override
+    public void doOnSuccess(Object result) {
+        Set<FetchUIDTaskPojo> addressBookContacts = null, botsContact = null, activeChats = null;
+
+        JSONObject data = (JSONObject) result;
+        addressBookContacts = parseJSONArrayIntoSet(addressBookContacts, data.optJSONArray("addr"));
+        botsContact = parseJSONArrayIntoSet(botsContact, data.optJSONArray("bots"));
+        activeChats = parseJSONArrayIntoSet(activeChats, data.optJSONArray("subs"));
+
+        //Update in User Db
+        HikeUserDatabase.getInstance().updateContactUid(addressBookContacts);
+
+        //Update in User Db.
+        HikeUserDatabase.getInstance().updateContactUid(activeChats);
+
+        //update Bots Table
+    }
+
+    @Override
+    public void doOnFailure(HttpException exception) {
+
+    }
+
+    @Override
+    public void onRequestFailure(HttpException httpException) {
+
+    }
+
+    @Override
+    public void onRequestSuccess(Response result) {
+        JSONObject response = (JSONObject) result.getBody().getContent();
+
+        if (Utils.isResponseValid(response)) {
+            doOnSuccess(response);
+        }
+
+    }
+
+    @Override
+    public void onRequestProgressUpdate(float progress) {
+
+    }
+
+    private void getMsisdnForMissingUID() {
+        activeChats = new HashSet<>();
+        //AddressBook Contact
+        addressBookContact = HikeUserDatabase.getInstance().getMsisdnsForMissingHikeUID();
+
+
+        //Active Chats
+
+        List<ContactInfo> list = (ContactManager.getInstance().getAllConversationContactsSorted(false, false));
+
+        for (ContactInfo ci : list) {
+            activeChats.add(ci.getMsisdn());
+        }
+        //Bots
+        bots = HikeMessengerApp.hikeBotInfoMap.keySet();
+
+
+    }
+}
