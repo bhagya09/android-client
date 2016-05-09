3e7f25bd6cf2433030620b53f1d191b0d4735114
Sidharth <sidharth@hike.in>
2016-05-09 12:16:37 +0530

    added TwinPrimeOkClientCustomDev and using preference in custom dev request runner

--- customDev/com/bsb/hike/modules/httpmgr/engine/RequestRunner.java
+++ customDev/com/bsb/hike/modules/httpmgr/engine/RequestRunner.java
@@ -1,11 +1,11 @@
 package com.bsb.hike.modules.httpmgr.engine;
 
+import com.bsb.hike.HikeConstants;
 import com.bsb.hike.modules.httpmgr.client.ClientOptions;
 import com.bsb.hike.modules.httpmgr.client.IClient;
-import com.bsb.hike.modules.httpmgr.exception.HttpException;
-import com.bsb.hike.modules.httpmgr.request.Request;
-import com.bsb.hike.modules.httpmgr.response.Response;
 import com.bsb.hike.modules.httpmgr.client.OkClientCustomDev;
+import com.bsb.hike.modules.httpmgr.client.TwinPrimeOkClientCustomDev;
+import com.bsb.hike.utils.HikeSharedPreferenceUtil;
 
 /**
  * This class clones the {@link IClient} object and passes it to {@link RequestExecuter} for the final execution of the request
@@ -22,6 +22,13 @@ public class RequestRunner extends com.bsb.hike.modules.httpmgr.engine.RequestRu
 
 	protected IClient getDefaultClient(ClientOptions options)
 	{
-		return new OkClientCustomDev(options);
+		if(HikeSharedPreferenceUtil.getInstance().getData(HikeConstants.TP_ENABLE,0)==1)
+		{
+			return new TwinPrimeOkClientCustomDev(options);
+		}
+		else
+		{
+			return new OkClientCustomDev(options);
+		}
 	}
 }
