771961fba9d34d7e176fbbcb8d6a9ab0580626fe
Sidharth <sidharth@hike.in>
2016-05-09 12:16:04 +0530

    using twinprimeokclient or okclient based on preference in arm request runner

--- arm/com/bsb/hike/modules/httpmgr/engine/RequestRunner.java
+++ arm/com/bsb/hike/modules/httpmgr/engine/RequestRunner.java
@@ -1,8 +1,11 @@
 package com.bsb.hike.modules.httpmgr.engine;
 
+import com.bsb.hike.HikeConstants;
 import com.bsb.hike.modules.httpmgr.client.ClientOptions;
 import com.bsb.hike.modules.httpmgr.client.IClient;
 import com.bsb.hike.modules.httpmgr.client.OkClient;
+import com.bsb.hike.modules.httpmgr.client.TwinPrimeOkClient;
+import com.bsb.hike.utils.HikeSharedPreferenceUtil;
 
 /**
  * This class clones the {@link IClient} object and passes it to {@link RequestExecuter} for the final execution of the request
@@ -19,6 +22,13 @@ public class RequestRunner extends RequestRunnerBase
 
 	protected IClient getDefaultClient(ClientOptions options)
 	{
-		return new OkClient(options);
+		if(HikeSharedPreferenceUtil.getInstance().getData(HikeConstants.TP_ENABLE,0)==1)
+		{
+			return new TwinPrimeOkClient(options);
+		}
+		else
+		{
+			return new OkClient(options);
+		}
 	}
 }
