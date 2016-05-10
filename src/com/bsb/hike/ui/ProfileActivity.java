a48dabc3c07b31a6113b403623bf3751d02e51fc
Sidharth <sidharth@hike.in>
2016-05-10 16:15:33 +0530

    marked previous saveChanges method as deprecated

--- src/com/bsb/hike/ui/ProfileActivity.java
+++ src/com/bsb/hike/ui/ProfileActivity.java
@@ -1839,6 +1839,7 @@ public class ProfileActivity extends ChangeProfileImageBaseActivity implements F
 		}
 	}
 
+	@Deprecated
 	public void saveChangesApache()
 	{
 		ArrayList<HikeHttpRequest> requests = new ArrayList<HikeHttpRequest>();
