d33af01e35e2608f794d7c5370c8dbafd4eb7edd
Atul Mittal <atul@hike.in>
2016-05-24 16:33:52 +0530

    intelliJ formatter

--- src/com/bsb/hike/timeline/adapter/StoryListAdapter.java
+++ src/com/bsb/hike/timeline/adapter/StoryListAdapter.java
@@ -1,7 +1,5 @@
 package com.bsb.hike.timeline.adapter;
 
-import java.util.List;
-
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
@@ -20,139 +18,116 @@ import com.bsb.hike.photos.HikePhotosUtils;
 import com.bsb.hike.timeline.model.StoryItem;
 import com.bsb.hike.view.PinnedSectionListView;
 
+import java.util.List;
+
 /**
  * Populates "friends/stories" tab content
- * 
+ * <p/>
  * Created by AtulM on 24/05/16.
  */
-public class StoryListAdapter extends BaseAdapter implements PinnedSectionListView.PinnedSectionListAdapter
-{
+public class StoryListAdapter extends BaseAdapter implements PinnedSectionListView.PinnedSectionListAdapter {
 
-	private final List<StoryItem> mStoryItemList;
+    private final List<StoryItem> mStoryItemList;
 
-	private final Context mContext;
+    private final Context mContext;
 
-	private LayoutInflater mInflater;
+    private LayoutInflater mInflater;
 
-	private class ViewHolder
-	{
-		public TextView titleView;
+    private class ViewHolder {
+        public TextView titleView;
 
-		public TextView subTextView;
+        public TextView subTextView;
 
-		public ImageView avatarView;
-	}
+        public ImageView avatarView;
+    }
 
-	public StoryListAdapter(List<StoryItem> argStoryItemList)
-	{
-		mStoryItemList = argStoryItemList;
-		mContext = HikeMessengerApp.getInstance().getApplicationContext();
-		mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	}
+    public StoryListAdapter(List<StoryItem> argStoryItemList) {
+        mStoryItemList = argStoryItemList;
+        mContext = HikeMessengerApp.getInstance().getApplicationContext();
+        mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
 
-	@Override
-	public int getCount()
-	{
-		return mStoryItemList.size();
-	}
+    @Override
+    public int getCount() {
+        return mStoryItemList.size();
+    }
 
-	@Override
-	public StoryItem getItem(int index)
-	{
-		return mStoryItemList.get(index);
-	}
+    @Override
+    public StoryItem getItem(int index) {
+        return mStoryItemList.get(index);
+    }
 
-	@Override
-	public long getItemId(int index)
-	{
-		return 0;
-	}
+    @Override
+    public long getItemId(int index) {
+        return 0;
+    }
 
-	@Override
-	public int getViewTypeCount()
-	{
-		// Header and non-header
-		return 2;
-	}
+    @Override
+    public int getViewTypeCount() {
+        // Header and non-header
+        return 2;
+    }
 
-	@Override
-	public int getItemViewType(int position)
-	{
-		return getItem(position).getType() == StoryItem.TYPE_HEADER ? StoryItem.TYPE_HEADER : StoryItem.TYPE_DEFAULT;
-	}
+    @Override
+    public int getItemViewType(int position) {
+        return getItem(position).getType() == StoryItem.TYPE_HEADER ? StoryItem.TYPE_HEADER : StoryItem.TYPE_DEFAULT;
+    }
 
-	@Override
-	public View getView(int index, View view, ViewGroup viewGroup)
-	{
-		StoryItem storyItem = getItem(index);
+    @Override
+    public View getView(int index, View view, ViewGroup viewGroup) {
+        StoryItem storyItem = getItem(index);
 
-		ViewHolder viewHolder = null;
+        ViewHolder viewHolder = null;
 
-		if (view == null)
-		{
+        if (view == null) {
 
-			if (storyItem.getType() == StoryItem.TYPE_HEADER)
-			{
+            if (storyItem.getType() == StoryItem.TYPE_HEADER) {
 
-			}
-			else if (storyItem.getType() == StoryItem.TYPE_INTENT || storyItem.getType() == StoryItem.TYPE_FRIEND || storyItem.getType() == StoryItem.TYPE_BRAND)
-			{
-				view = mInflater.inflate(R.layout.list_item_story, null, false);
-				viewHolder = new ViewHolder();
-				viewHolder.avatarView = (ImageView) view.findViewById(R.id.avatar);
-				viewHolder.titleView = (TextView) view.findViewById(R.id.title);
-				viewHolder.subTextView = (TextView) view.findViewById(R.id.subtext);
-				view.setTag(viewHolder);
-			}
-		}
-		else
-		{
-			viewHolder = (ViewHolder) view.getTag();
-		}
+            } else if (storyItem.getType() == StoryItem.TYPE_INTENT || storyItem.getType() == StoryItem.TYPE_FRIEND || storyItem.getType() == StoryItem.TYPE_BRAND) {
+                view = mInflater.inflate(R.layout.list_item_story, null, false);
+                viewHolder = new ViewHolder();
+                viewHolder.avatarView = (ImageView) view.findViewById(R.id.avatar);
+                viewHolder.titleView = (TextView) view.findViewById(R.id.title);
+                viewHolder.subTextView = (TextView) view.findViewById(R.id.subtext);
+                view.setTag(viewHolder);
+            }
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
 
-		if (storyItem.getType() == StoryItem.TYPE_HEADER)
-		{
+        if (storyItem.getType() == StoryItem.TYPE_HEADER) {
 
-		}
-		else if (storyItem.getType() == StoryItem.TYPE_INTENT)
-		{
-			Drawable timelineLogoDrawable = ContextCompat.getDrawable(mContext, R.drawable.ic_dp_timeline);
-			Drawable otherFeaturesDrawable = ContextCompat.getDrawable(mContext, R.drawable.other_features_bg);
-			viewHolder.avatarView.setImageDrawable(timelineLogoDrawable);
-			viewHolder.avatarView.setBackground(otherFeaturesDrawable);
-			viewHolder.titleView.setText(storyItem.getTitle());
+        } else if (storyItem.getType() == StoryItem.TYPE_INTENT) {
+            Drawable timelineLogoDrawable = ContextCompat.getDrawable(mContext, R.drawable.ic_dp_timeline);
+            Drawable otherFeaturesDrawable = ContextCompat.getDrawable(mContext, R.drawable.other_features_bg);
+            viewHolder.avatarView.setImageDrawable(timelineLogoDrawable);
+            viewHolder.avatarView.setBackground(otherFeaturesDrawable);
+            viewHolder.titleView.setText(storyItem.getTitle());
 
-			String subText = storyItem.getSubText();
-			if (!TextUtils.isEmpty(subText))
-			{
-				viewHolder.subTextView.setText(subText);
-				viewHolder.subTextView.setVisibility(View.VISIBLE);
-			}
-		}
-		else if (storyItem.getType() == StoryItem.TYPE_FRIEND)
-		{
+            String subText = storyItem.getSubText();
+            if (!TextUtils.isEmpty(subText)) {
+                viewHolder.subTextView.setText(subText);
+                viewHolder.subTextView.setVisibility(View.VISIBLE);
+            }
+        } else if (storyItem.getType() == StoryItem.TYPE_FRIEND) {
 
-		}
-		else if (storyItem.getType() == StoryItem.TYPE_BRAND)
-		{
-			// TODO
-		}
+        } else if (storyItem.getType() == StoryItem.TYPE_BRAND) {
+            // TODO
+        }
 
-		if (index == 0)
-		{
-			LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) viewHolder.avatarView.getLayoutParams();
-			params.topMargin = HikePhotosUtils.dpToPx(22);
-			params.bottomMargin = HikePhotosUtils.dpToPx(20);
-			viewHolder.avatarView.setLayoutParams(params);
-		}
+        if (index == 0) {
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) viewHolder.avatarView.getLayoutParams();
+            params.topMargin = HikePhotosUtils.dpToPx(22);
+            params.bottomMargin = HikePhotosUtils.dpToPx(20);
+            viewHolder.avatarView.setLayoutParams(params);
+        }
 
-		return view;
+        return view;
 
-	}
+    }
 
-	@Override
-	public boolean isItemViewTypePinned(int viewType)
-	{
-		return viewType == StoryItem.TYPE_HEADER ? true : false;
-	}
+    @Override
+    public boolean isItemViewTypePinned(int viewType) {
+        return viewType == StoryItem.TYPE_HEADER ? true : false;
+    }
 }
--- src/com/bsb/hike/timeline/model/StoryItem.java
+++ src/com/bsb/hike/timeline/model/StoryItem.java
@@ -1,120 +1,104 @@
 package com.bsb.hike.timeline.model;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
 import android.content.Intent;
 import android.support.annotation.IntDef;
 import android.support.annotation.NonNull;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Building blocks of "stories" listview
- * 
+ * <p/>
  * Created by AtulM on 24/05/16.
  */
-public class StoryItem
-{
-	private int mCategory = CATEGORY_NONE;
+public class StoryItem {
+    private int mCategory = CATEGORY_NONE;
 
-	private int mType;
+    private int mType;
 
-	private String mName;
+    private String mName;
 
-	private String mSubText;
+    private String mSubText;
 
-	private Intent mIntent;
+    private Intent mIntent;
 
-	@IntDef({ TYPE_INTENT, TYPE_FRIEND, TYPE_BRAND, TYPE_HEADER })
-	@Retention(RetentionPolicy.SOURCE)
-	public @interface StoryItemType
-	{
-	}
+    @IntDef({TYPE_INTENT, TYPE_FRIEND, TYPE_BRAND, TYPE_HEADER})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface StoryItemType {
+    }
 
-	// Fire associated intent
-	public static final int TYPE_INTENT = 0;
+    // Fire associated intent
+    public static final int TYPE_INTENT = 0;
 
-	// Open friends story
-	public static final int TYPE_FRIEND = 1;
+    // Open friends story
+    public static final int TYPE_FRIEND = 1;
 
-	// Open brand story? TODO
-	public static final int TYPE_BRAND = 2;
+    // Open brand story? TODO
+    public static final int TYPE_BRAND = 2;
 
-	// Serves as header in StoryItemList
-	public static final int TYPE_HEADER = 3;
+    // Serves as header in StoryItemList
+    public static final int TYPE_HEADER = 3;
 
-	// Generalization of TYPE_INTENT, TYPE_FRIEND and TYPE_BRAND. Used to distinguish between header/non-header in adapters.
-	public static final int TYPE_DEFAULT = 4;
+    // Generalization of TYPE_INTENT, TYPE_FRIEND and TYPE_BRAND. Used to distinguish between header/non-header in adapters.
+    public static final int TYPE_DEFAULT = 4;
 
-	@IntDef({ CATEGORY_NONE, CATEGORY_RECENT, CATEGORY_ALL, CATEGORY_DEFAULT })
-	@Retention(RetentionPolicy.SOURCE)
-	public @interface StoryCategory
-	{
-	}
+    @IntDef({CATEGORY_NONE, CATEGORY_RECENT, CATEGORY_ALL, CATEGORY_DEFAULT})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface StoryCategory {
+    }
 
-	public static final int CATEGORY_NONE = 11;
+    public static final int CATEGORY_NONE = 11;
 
-	public static final int CATEGORY_RECENT = 12;
+    public static final int CATEGORY_RECENT = 12;
 
-	public static final int CATEGORY_ALL = 13;
+    public static final int CATEGORY_ALL = 13;
 
-	public static final int CATEGORY_DEFAULT = 14;
+    public static final int CATEGORY_DEFAULT = 14;
 
-	public StoryItem(@StoryItemType int argType, @NonNull String argName)
-	{
-		mType = argType;
-		mName = argName;
-	}
+    public StoryItem(@StoryItemType int argType, @NonNull String argName) {
+        mType = argType;
+        mName = argName;
+    }
 
-	public void setCategory(@StoryCategory int argCategory)
-	{
-		mCategory = argCategory;
-	}
+    public void setCategory(@StoryCategory int argCategory) {
+        mCategory = argCategory;
+    }
 
-	public void setSubText(String argSubText)
-	{
-		mSubText = argSubText;
-	}
+    public void setSubText(String argSubText) {
+        mSubText = argSubText;
+    }
 
-	public void setIntent(@NonNull Intent argIntent)
-	{
-		mIntent = argIntent;
-	}
+    public void setIntent(@NonNull Intent argIntent) {
+        mIntent = argIntent;
+    }
 
-	public int getType()
-	{
-		return mType;
-	}
+    public int getType() {
+        return mType;
+    }
 
-	public String getTitle()
-	{
-		return mName;
-	}
+    public String getTitle() {
+        return mName;
+    }
 
-	public String getSubText()
-	{
-		return mSubText;
-	}
+    public String getSubText() {
+        return mSubText;
+    }
 
-	public Intent getIntent()
-	{
-		return mIntent;
-	}
+    public Intent getIntent() {
+        return mIntent;
+    }
 
-	public int getCategory()
-	{
-		return mCategory;
-	}
+    public int getCategory() {
+        return mCategory;
+    }
 
-	public boolean isRead()
-	{
-		if (mCategory == CATEGORY_ALL || mCategory == CATEGORY_DEFAULT)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
+    public boolean isRead() {
+        if (mCategory == CATEGORY_ALL || mCategory == CATEGORY_DEFAULT) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
