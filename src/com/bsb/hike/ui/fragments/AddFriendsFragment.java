e6f331b0426a019f6167a24b3af3cb9411a44c90
gaurav-m <gauravm@bsb.in>
2016-05-27 14:24:18 +0530

    formatting again

--- src/com/bsb/hike/ui/fragments/AddFriendsFragment.java
+++ src/com/bsb/hike/ui/fragments/AddFriendsFragment.java
@@ -1,16 +1,5 @@
 package com.bsb.hike.ui.fragments;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.bsb.hike.R;
-import com.bsb.hike.adapters.FriendRequestAdapter;
-import com.bsb.hike.chatthread.ChatThreadActivity;
-import com.bsb.hike.models.ContactInfo;
-import com.bsb.hike.modules.contactmgr.ContactManager;
-import com.bsb.hike.utils.IntentFactory;
-import com.bsb.hike.utils.Utils;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -27,137 +16,128 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.bsb.hike.R;
+import com.bsb.hike.adapters.FriendRequestAdapter;
+import com.bsb.hike.chatthread.ChatThreadActivity;
+import com.bsb.hike.models.ContactInfo;
+import com.bsb.hike.modules.contactmgr.ContactManager;
+import com.bsb.hike.utils.IntentFactory;
+import com.bsb.hike.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by gauravmittal on 19/05/16.
  */
-public class AddFriendsFragment extends ListFragment
-{
+public class AddFriendsFragment extends ListFragment {
 
-	private ListView listView;
+    private ListView listView;
 
-	private MenuItem searchMenuItem;
+    private MenuItem searchMenuItem;
 
-	private FriendRequestAdapter mAdapter;
+    private FriendRequestAdapter mAdapter;
 
-	private List<ContactInfo> getToAddContactList()
-	{
-		List<ContactInfo> allContacts = ContactManager.getInstance().getAllContacts(true);
-		List<ContactInfo> toAddcontacts = new ArrayList<>();
-		for (ContactInfo info : allContacts)
-		{
-			if (!info.isBot() && !info.isMyOneWayFriend())
-			{
-				toAddcontacts.add(info);
-			}
-		}
-		// so it gets GC-ed
-		allContacts.clear();
-		return toAddcontacts;
-	}
+    private List<ContactInfo> getToAddContactList() {
+        List<ContactInfo> allContacts = ContactManager.getInstance().getAllContacts(true);
+        List<ContactInfo> toAddcontacts = new ArrayList<>();
+        for (ContactInfo info : allContacts) {
+            if (!info.isBot() && !info.isMyOneWayFriend()) {
+                toAddcontacts.add(info);
+            }
+        }
+        // so it gets GC-ed
+        allContacts.clear();
+        return toAddcontacts;
+    }
 
-	@Nullable
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-	{
-		setHasOptionsMenu(true);
-		View parent = inflater.inflate(R.layout.fragment_friend_request, null);
-		mAdapter = new FriendRequestAdapter(getToAddContactList(), getActivity());
-		return parent;
-	}
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        View parent = inflater.inflate(R.layout.fragment_friend_request, null);
+        mAdapter = new FriendRequestAdapter(getToAddContactList(), getActivity());
+        return parent;
+    }
 
-	@Override
-	public void onViewCreated(View view, Bundle savedInstanceState)
-	{
-		super.onViewCreated(view, savedInstanceState);
-		listView = getListView();
-		listView.setAdapter(mAdapter);
-		listView.setOnItemClickListener(onItemClickListener);
-	}
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        listView = getListView();
+        listView.setAdapter(mAdapter);
+        listView.setOnItemClickListener(onItemClickListener);
+    }
 
-	private AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener()
-	{
-		@Override
-		public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-		{
-			ContactInfo contact = mAdapter.getItem(position);
-			Intent intent = IntentFactory.createChatThreadIntentFromContactInfo(getContext(), contact, false, false, ChatThreadActivity.ChatThreadOpenSources.ADD_FRIEND_FRAG);
-			startActivity(intent);
-		}
-	};
+    private AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            ContactInfo contact = mAdapter.getItem(position);
+            Intent intent = IntentFactory.createChatThreadIntentFromContactInfo(getContext(), contact, false, false, ChatThreadActivity.ChatThreadOpenSources.ADD_FRIEND_FRAG);
+            startActivity(intent);
+        }
+    };
 
-	@Override
-	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
-	{
-		menu.clear();
-		inflater.inflate(R.menu.friend_req_menu, menu);
-		setupSearchOptionItem(menu);
-		super.onCreateOptionsMenu(menu, inflater);
-	}
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.clear();
+        inflater.inflate(R.menu.friend_req_menu, menu);
+        setupSearchOptionItem(menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 
-	private void setupSearchOptionItem(final Menu menu)
-	{
-		searchMenuItem = menu.findItem(R.id.search);
-		SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchMenuItem);
-		searchView.setOnQueryTextListener(onQueryTextListener);
-		searchView.setQueryHint(getString(R.string.search));
-		searchView.clearFocus();
+    private void setupSearchOptionItem(final Menu menu) {
+        searchMenuItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchMenuItem);
+        searchView.setOnQueryTextListener(onQueryTextListener);
+        searchView.setQueryHint(getString(R.string.search));
+        searchView.clearFocus();
 
-		MenuItemCompat.setOnActionExpandListener(searchMenuItem, new MenuItemCompat.OnActionExpandListener()
-		{
-			@Override
-			public boolean onMenuItemActionExpand(MenuItem item)
-			{
-				setupSearch();
-				return true;
-			}
+        MenuItemCompat.setOnActionExpandListener(searchMenuItem, new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                setupSearch();
+                return true;
+            }
 
-			@Override
-			public boolean onMenuItemActionCollapse(MenuItem item)
-			{
-				endSearch();
-				return true;
-			}
-		});
-	}
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                endSearch();
+                return true;
+            }
+        });
+    }
 
-	private SearchView.OnQueryTextListener onQueryTextListener = new SearchView.OnQueryTextListener()
-	{
-		@Override
-		public boolean onQueryTextSubmit(String query)
-		{
-			Utils.hideSoftKeyboard(getActivity().getApplicationContext(), searchMenuItem.getActionView());
-			return true;
-		}
+    private SearchView.OnQueryTextListener onQueryTextListener = new SearchView.OnQueryTextListener() {
+        @Override
+        public boolean onQueryTextSubmit(String query) {
+            Utils.hideSoftKeyboard(getActivity().getApplicationContext(), searchMenuItem.getActionView());
+            return true;
+        }
 
-		@Override
-		public boolean onQueryTextChange(String newText)
-		{
-			if (!TextUtils.isEmpty(newText))
-				newText = newText.toLowerCase().trim();
-			mAdapter.onSearchQueryChanged(newText, null);
-			return true;
-		}
-	};
+        @Override
+        public boolean onQueryTextChange(String newText) {
+            if (!TextUtils.isEmpty(newText))
+                newText = newText.toLowerCase().trim();
+            mAdapter.onSearchQueryChanged(newText, null);
+            return true;
+        }
+    };
 
-	public boolean isSearchModeOn()
-	{
-		return searchMenuItem.isActionViewExpanded();
-	}
+    public boolean isSearchModeOn() {
+        return searchMenuItem.isActionViewExpanded();
+    }
 
-	private void setupSearch()
-	{
-		mAdapter.setupSearchMode();
-	}
+    private void setupSearch() {
+        mAdapter.setupSearchMode();
+    }
 
-	private void endSearch()
-	{
-		mAdapter.endSearchMode();
+    private void endSearch() {
+        mAdapter.endSearchMode();
 
-	}
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 }
