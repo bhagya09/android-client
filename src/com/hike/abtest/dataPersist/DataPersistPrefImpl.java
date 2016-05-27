a96c7c1124da5d907858fbbef2ef0ab981c318ce
Abhijith Krishnappa <abhijith@hike.in>
2016-05-27 09:47:38 +0530

    Auto-Indent ABTesting FWK. NO manula changes

--- src/com/hike/abtest/dataPersist/DataPersistPrefImpl.java
+++ src/com/hike/abtest/dataPersist/DataPersistPrefImpl.java
@@ -16,7 +16,7 @@ import com.hike.abtest.dataparser.ProtoMapper;
  * SharedPreference implementation of persistence interface
  * Created by abhijithkrishnappa on 11/04/16.
  */
-public class DataPersistPrefImpl extends DataPersist{
+public class DataPersistPrefImpl extends DataPersist {
     private static final String EXPERIMENTS_STORE = "abtest_experiments_store";
     private static final String TAG = DataPersistPrefImpl.class.getSimpleName();
     SharedPreferences mExperimentsPref;
@@ -30,7 +30,7 @@ public class DataPersistPrefImpl extends DataPersist{
         SharedPreferences.Editor mExpEditor = mExperimentsPref.edit();
         for (Map.Entry<String, String> entry : experimentMap.entrySet()) {
             Logger.d(TAG, entry.getKey() + "/" + entry.getValue());
-            if(!mExperimentsPref.contains(entry.getKey())) {
+            if (!mExperimentsPref.contains(entry.getKey())) {
                 mExpEditor.putString(entry.getKey(), entry.getValue());
             }
         }
@@ -50,8 +50,8 @@ public class DataPersistPrefImpl extends DataPersist{
 
     public void abortExperiment(List<String> experimentIds) {
         SharedPreferences.Editor mExpEditor = mExperimentsPref.edit();
-        for(String experimentId : experimentIds) {
-            if(mExperimentsPref.contains(experimentId)) {
+        for (String experimentId : experimentIds) {
+            if (mExperimentsPref.contains(experimentId)) {
                 mExpEditor.remove(experimentId);
                 Logger.d(TAG, "Removed experiment: " + experimentId);
             }
