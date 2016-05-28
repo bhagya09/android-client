05e43d6898049876c8acaaf26fd9663216ade333
piyushghai <piyush@hike.in>
2016-05-28 23:26:17 +0530

    Made some changes to remove unused vars from EXplandableListItem

--- src/com/bsb/hike/ExplandingCells/ExpandableListItem.java
+++ src/com/bsb/hike/ExplandingCells/ExpandableListItem.java
@@ -10,19 +10,13 @@ package com.bsb.hike.ExplandingCells;
 
 public class ExpandableListItem implements OnSizeChangedListener {
 
-    private String mTitle;
-    private String mText;
     private boolean mIsExpanded;
-    private int mImgResource;
     private int mCollapsedHeight;
     private int mExpandedHeight;
 
-    public ExpandableListItem(String title, int imgResource, int collapsedHeight, String text) {
-        mTitle = title;
-        mImgResource = imgResource;
+    public ExpandableListItem(int collapsedHeight) {
         mCollapsedHeight = collapsedHeight;
         mIsExpanded = false;
-        mText = text;
         mExpandedHeight = -1;
     }
 
@@ -34,32 +28,8 @@ public class ExpandableListItem implements OnSizeChangedListener {
         mIsExpanded = isExpanded;
     }
 
-    public String getTitle() {
-        return mTitle;
-    }
-
-    public int getImgResource() {
-        return mImgResource;
-    }
-
     public int getCollapsedHeight() {
         return mCollapsedHeight;
-    }
-
-    public void setCollapsedHeight(int collapsedHeight) {
-        mCollapsedHeight = collapsedHeight;
-    }
-
-    public String getText() {
-        return mText;
-    }
-
-    public void setText(String text) {
-        mText = text;
-    }
-
-    public int getExpandedHeight() {
-        return mExpandedHeight;
     }
 
     public void setExpandedHeight(int expandedHeight) {
