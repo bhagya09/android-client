8cd182a4c6e8cfbb06f0e46451338bc57cb1c582
Atul Mittal <atul@hike.in>
2016-06-02 15:00:34 +0530

    bug fix in loading icons

--- src/com/bsb/hike/timeline/adapter/StoryListAdapter.java
+++ src/com/bsb/hike/timeline/adapter/StoryListAdapter.java
@@ -201,7 +201,7 @@ public class StoryListAdapter extends BaseAdapter implements PinnedSectionListVi
                     //Load last photo post
                     RoundedImageView roundImageView = (RoundedImageView) viewHolder.avatarView;
                     roundImageView.setOval(true);
-                    mTimelineImageLoader.loadImage(statusMessagesList.get(0).getMappedId() + "_icon", viewHolder.avatarView, false, false, false, statusMessagesList.get(0));
+                    mTimelineImageLoader.loadImage(statusMessagesList.get(0).getMappedId() + TimelineUpdatesImageLoader.SMALL_ICON_ID_SUFFIX, viewHolder.avatarView, false, false, false, statusMessagesList.get(0));
                 }
             } else {
                 Logger.wtf(TAG, "Friends story item but no stories attached!!");
--- src/com/bsb/hike/timeline/view/StoryFragment.java
+++ src/com/bsb/hike/timeline/view/StoryFragment.java
@@ -54,7 +54,7 @@ public class StoryFragment extends Fragment implements View.OnClickListener, Hik
 
     private View btnAddFriends;
 
-    private final String[] pubsubEvents = new String[]{HikePubSub.UNSEEN_STATUS_COUNT_CHANGED, HikePubSub.TIMELINE_UPDATE_RECIEVED, HikePubSub.ICON_CHANGED, HikePubSub.ACTIVITY_UPDATE, HikePubSub.STATUS_MARKED_READ, HikePubSub.STEALTH_MODE_TOGGLED};
+    private final String[] pubsubEvents = new String[]{HikePubSub.UNSEEN_STATUS_COUNT_CHANGED, HikePubSub.TIMELINE_UPDATE_RECIEVED, HikePubSub.ICON_CHANGED, HikePubSub.ACTIVITY_UPDATE, HikePubSub.STATUS_MARKED_READ, HikePubSub.STEALTH_MODE_TOGGLED, HikePubSub.DELETE_STATUS};
 
     public static StoryFragment newInstance(@Nullable Bundle argBundle) {
         StoryFragment fragmentInstance = new StoryFragment();
@@ -188,7 +188,8 @@ public class StoryFragment extends Fragment implements View.OnClickListener, Hik
 
     @Override
     public void onEventReceived(String type, Object object) {
-        if (type.equals(HikePubSub.UNSEEN_STATUS_COUNT_CHANGED) || type.equals(HikePubSub.ACTIVITY_UPDATE)) {
+        if (type.equals(HikePubSub.UNSEEN_STATUS_COUNT_CHANGED)
+                || type.equals(HikePubSub.ACTIVITY_UPDATE)) {
             if (isAdded() && getActivity() != null) {
                 HikeHandlerUtil.getInstance().postRunnableWithDelay(new Runnable() {
                     @Override
@@ -197,7 +198,10 @@ public class StoryFragment extends Fragment implements View.OnClickListener, Hik
                     }
                 }, 2000); // This is to avoid changing of subtext right when timeline is tapped since it takes time for timeline activity to show up
             }
-        } else if (type.equals(HikePubSub.TIMELINE_UPDATE_RECIEVED) || type.equals(HikePubSub.STATUS_MARKED_READ) || type.equals(HikePubSub.STEALTH_MODE_TOGGLED)) {
+        } else if (type.equals(HikePubSub.TIMELINE_UPDATE_RECIEVED)
+                || type.equals(HikePubSub.STATUS_MARKED_READ)
+                || type.equals(HikePubSub.STEALTH_MODE_TOGGLED)
+                || type.equals(HikePubSub.DELETE_STATUS)) {
             if (isAdded() && getActivity() != null) {
                 HikeHandlerUtil.getInstance().postRunnable(new Runnable() {
                     @Override
