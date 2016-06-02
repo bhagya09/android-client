108dcc6d9d07e66e104ac15e7f9d99a13d0b291d
Atul Mittal <atul@hike.in>
2016-06-02 15:11:09 +0530

    order status messages in desc timestamp

--- src/com/bsb/hike/db/HikeConversationsDatabase.java
+++ src/com/bsb/hike/db/HikeConversationsDatabase.java
@@ -11145,7 +11145,7 @@ public class HikeConversationsDatabase extends SQLiteOpenHelper
 
 			Cursor c = null;
 			try {
-				c = mDb.query(DBConstants.STATUS_TABLE, columns, selection, null, null, null, null);
+				c = mDb.query(DBConstants.STATUS_TABLE, columns, selection, null, null, null, DBConstants.TIMESTAMP +" DESC");
 
 				List<StatusMessage> statusMessages = new ArrayList<StatusMessage>(c.getCount());
 				Map<String, List<StatusMessage>> statusMessagesMap = new HashMap<String, List<StatusMessage>>();
