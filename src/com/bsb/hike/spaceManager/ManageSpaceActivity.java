58ac98d6be869c5a0de4783c215858e53f8ae405
Param Shah <param@hike.in>
2016-06-09 20:05:26 +0530

    empty view cleanup

--- src/com/bsb/hike/spaceManager/ManageSpaceActivity.java
+++ src/com/bsb/hike/spaceManager/ManageSpaceActivity.java
@@ -311,7 +311,6 @@ public class ManageSpaceActivity extends HikeAppStateBaseFragmentActivity implem
     {
         if(spaceManagerItems.isEmpty())
         {
-            emptyLayoutViewStub.inflate();
             emptyLayoutViewStub.setVisibility(View.VISIBLE);
             manageSpaceListView.setVisibility(View.GONE);
             doneBtn.setVisibility(View.GONE);
