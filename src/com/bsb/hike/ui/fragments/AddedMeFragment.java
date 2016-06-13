ee9bd2a8f2723d040781e3baca24d8288672687c
piyushghai <piyush@hike.in>
2016-06-13 18:15:37 +0530

    Removed search from AddedMeFrag

--- src/com/bsb/hike/ui/fragments/AddedMeFragment.java
+++ src/com/bsb/hike/ui/fragments/AddedMeFragment.java
@@ -4,13 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.widget.SearchView;
-import android.text.TextUtils;
 import android.util.Pair;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,7 +21,6 @@ import com.bsb.hike.models.ContactInfo;
 import com.bsb.hike.modules.contactmgr.ContactManager;
 import com.bsb.hike.utils.HikeSharedPreferenceUtil;
 import com.bsb.hike.utils.IntentFactory;
-import com.bsb.hike.utils.Utils;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -41,8 +35,6 @@ public class AddedMeFragment extends ListFragment implements HikePubSub.Listener
     private static final String ADDED_ME_BADGE_COUNT = "added_me_badge_count";
 
     private ListView listView;
-
-    private MenuItem searchMenuItem;
 
     private FriendRequestAdapter mAdapter;
 
@@ -74,7 +66,6 @@ public class AddedMeFragment extends ListFragment implements HikePubSub.Listener
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        setHasOptionsMenu(true);
         View parent = inflater.inflate(R.layout.fragment_added_me, null);
         mAdapter = new FriendRequestAdapter(setupAddedMeContactList(), getActivity());
         markFriendsRead();
@@ -107,65 +98,6 @@ public class AddedMeFragment extends ListFragment implements HikePubSub.Listener
             startActivity(intent);
         }
     };
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        menu.clear();
-        inflater.inflate(R.menu.friend_req_menu, menu);
-        setupSearchOptionItem(menu);
-        super.onCreateOptionsMenu(menu, inflater);
-    }
-
-    private void setupSearchOptionItem(final Menu menu) {
-        searchMenuItem = menu.findItem(R.id.search);
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchMenuItem);
-        searchView.setOnQueryTextListener(onQueryTextListener);
-        searchView.setQueryHint(getString(R.string.search));
-        searchView.clearFocus();
-
-        MenuItemCompat.setOnActionExpandListener(searchMenuItem, new MenuItemCompat.OnActionExpandListener() {
-            @Override
-            public boolean onMenuItemActionExpand(MenuItem item) {
-                setupSearch();
-                return true;
-            }
-
-            @Override
-            public boolean onMenuItemActionCollapse(MenuItem item) {
-                endSearch();
-                return true;
-            }
-        });
-    }
-
-    private SearchView.OnQueryTextListener onQueryTextListener = new SearchView.OnQueryTextListener() {
-        @Override
-        public boolean onQueryTextSubmit(String query) {
-            Utils.hideSoftKeyboard(getActivity().getApplicationContext(), searchMenuItem.getActionView());
-            return true;
-        }
-
-        @Override
-        public boolean onQueryTextChange(String newText) {
-            if (!TextUtils.isEmpty(newText))
-                newText = newText.toLowerCase().trim();
-            mAdapter.onSearchQueryChanged(newText, null);
-            return true;
-        }
-    };
-
-    public boolean isSearchModeOn() {
-        return searchMenuItem.isActionViewExpanded();
-    }
-
-    private void setupSearch() {
-        mAdapter.setupSearchMode();
-    }
-
-    private void endSearch() {
-        mAdapter.endSearchMode();
-
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
