b839c82eea928f80f94ebce6f0879713d59454ab
Varun Arora <varuna@hike.in>
2016-06-20 01:01:34 +0530

    CD-949

--- src/com/bsb/hike/chatthread/ChatThread.java
+++ src/com/bsb/hike/chatthread/ChatThread.java
@@ -3208,7 +3208,7 @@ import static com.bsb.hike.HikeConstants.IntentAction.ACTION_KEYBOARD_CLOSED;
 						if(metadata != null){
 
 							if (metadata.optJSONArray(HikeConstants.FILES) != null && metadata.optJSONArray(HikeConstants.FILES).length() > 0) {
-									handleFileTypeMessage(intent, metadata, msgExtrasJson.optString(HikeConstants.HIKE_MESSAGE), msgExtrasJson.optString(HikeConstants.Extras.FILE_PATH));
+									handleFileTypeMessage(intent, metadata, msgExtrasJson.optString(HikeConstants.HIKE_MESSAGE));
 							} else {
 								sendNativeCardMessage(msgExtrasJson);
 							}
@@ -3385,8 +3385,7 @@ import static com.bsb.hike.HikeConstants.IntentAction.ACTION_KEYBOARD_CLOSED;
 		convMessage.setMessage(msgExtrasJson.optString(HikeConstants.HIKE_MESSAGE));
 		sendMessage(convMessage);
 	}
-	private void handleFileTypeMessage(Intent intent, JSONObject nativeCardMetadata, String hikeMessage, String filePath) throws JSONException{
-        //TODO: parse from HikeFile
+	private void handleFileTypeMessage(Intent intent, JSONObject nativeCardMetadata, String hikeMessage) throws JSONException{
 		JSONArray filesArray = nativeCardMetadata.optJSONArray(HikeConstants.FILES);
 		JSONObject msg = filesArray.getJSONObject(0);
 		String fileKey = null;
@@ -3407,9 +3406,12 @@ import static com.bsb.hike.HikeConstants.IntentAction.ACTION_KEYBOARD_CLOSED;
 			attachmentType = FTAnalyticEvents.FILE_ATTACHEMENT;
 
 		}
-
+		String fileName = msg.getString(HikeConstants.FILE_NAME);
 		HikeFileType hikeFileType = HikeFileType.fromString(fileType);
-
+		//CD-949
+		// Since this is a native card message, hence getting the file path from the received message(received via bot like JFL)
+		File file = Utils.getOutputMediaFile(hikeFileType, fileName, false);
+		String filePath = file.getPath();
 		Logger.d("ChatThread", "isCloudMediaUri" + Utils.isPicasaUri(filePath));
 
 		ChatThreadUtils.initialiseFileTransferForNativeCards(activity.getApplicationContext(), msisdn, filePath, fileKey, hikeFileType, fileType, false,
